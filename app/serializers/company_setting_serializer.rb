24cd3856e8274e3fa12b1e80ccf3c0cae3277794
malcomowilla <owillamalcom@gmail.com>
2025-01-28 17:56:53 +0300

    logo url7

--- app/serializers/company_setting_serializer.rb
+++ app/serializers/company_setting_serializer.rb
@@ -6,22 +6,12 @@ class CompanySettingSerializer < ActiveModel::Serializer
 
 
 
+  
+
+
   include Rails.application.routes.url_helpers
   include ActionView::Helpers::AssetUrlHelper
   attributes :id, :company_name, :contact_info, :email_info, :logo, 
   :customer_support_phone_number, :agent_email, :customer_support_email
-
-
-
-
-  def logo
-
-    
-    return unless object.logo.attached?
-
-    # Generate the full URL for the logo
-    Rails.application.routes.url_helpers.rails_blob_url(object.logo, host: '102.221.35.116')
-  end
-
 
 end
