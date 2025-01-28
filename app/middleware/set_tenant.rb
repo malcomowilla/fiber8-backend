cc22a840db06422712a469828436338c6b833181
malcomowilla <owillamalcom@gmail.com>
2025-01-28 09:36:31 +0300

    hotspot network deployment19

--- app/middleware/set_tenant.rb
+++ app/middleware/set_tenant.rb
@@ -1,3 +1,42 @@
+# class SetTenant
+#   def initialize(app)
+#     @app = app
+#   end
+
+#   def call(env)
+#     request = ActionDispatch::Request.new(env)
+#     Rails.logger.info "Request object domain: #{request.domain}"
+#     Rails.logger.info "Request object subdomain: #{request.subdomain}"
+  
+#     # Read the subdomain from the custom header
+#     host = request.headers['X-Subdomain']
+#     Rails.logger.info "Subdomain from header: #{host}"
+  
+#     if host = request.headers['X-Subdomain']
+#       Rails.logger.info "Setting tenant for host: #{host}"
+  
+#       begin
+#         # Find or create the account based on the subdomain
+#         account = Account.find_or_create_by(subdomain: host)
+#         ActsAsTenant.current_tenant = account
+#         Rails.logger.info "Tenant set to: #{account.subdomain}"
+#       rescue => e
+#         # Log the error and continue execution
+#         Rails.logger.error "Failed to set tenant for host #{host}: #{e.message}"
+#         Rails.logger.error e.backtrace.join("\n") # Log the full backtrace for debugging
+#       end
+#     else
+#       Rails.logger.warn "No subdomain found in the request headers."
+#     end
+  
+#     # Call the next middleware or application
+#     @app.call(env)
+#   end
+# end
+
+
+
+
 class SetTenant
   def initialize(app)
     @app = app
@@ -5,16 +44,15 @@ class SetTenant
 
   def call(env)
     request = ActionDispatch::Request.new(env)
-    Rails.logger.info "Request object domain: #{request.domain}"
-    Rails.logger.info "Request object subdomain: #{request.subdomain}"
-  
-    # Read the subdomain from the custom header
+    Rails.logger.info "Request headers: #{request.headers.to_h}" # Log all headers
+
+    # Extract subdomain from the custom header or use default logic
     host = request.headers['X-Subdomain']
     Rails.logger.info "Subdomain from header: #{host}"
-  
-    if host = request.headers['X-Subdomain']
+
+    if host.present?
       Rails.logger.info "Setting tenant for host: #{host}"
-  
+
       begin
         # Find or create the account based on the subdomain
         account = Account.find_or_create_by(subdomain: host)
@@ -28,13 +66,11 @@ class SetTenant
     else
       Rails.logger.warn "No subdomain found in the request headers."
     end
-  
+
     # Call the next middleware or application
     @app.call(env)
   end
 end
-
-
 
 
 
