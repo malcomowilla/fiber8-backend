fef05adabf7da6ca9ddd55db3ba9f1e34be69f80
malcomowilla <owillamalcom@gmail.com>
2025-01-28 17:39:30 +0300

    logo url5

--- app/controllers/company_settings_controller.rb
+++ app/controllers/company_settings_controller.rb
@@ -15,9 +15,17 @@ class CompanySettingsController < ApplicationController
       customer_support_email: @company_settings&.customer_support_email,
       customer_support_phone_number: @company_settings&.customer_support_phone_number,
       # logo_url: @company_settings&.logo&.attached? ? url_for(@company_settings.logo) : nil,
-      logo_url: @company_settings&.logo&.attached? ? rails_blob_url(@company_settings.logo, host: '102.221.35.116') : nil
+      logo_url: @company_settings&.logo&.attached? ? rails_blob_url(@company_settings.logo, host: '102.221.35.116',
+      
+      protocol: 'https'
+      ) : nil
 
       }
+
+      
+
+
+
 
   end
 
