33ca5bdb0caea2db186a9311c0b0847f338d1203
malcomowilla <owillamalcom@gmail.com>
2025-01-30 17:27:25 +0300

    https rails3

--- app/controllers/company_settings_controller.rb
+++ app/controllers/company_settings_controller.rb
@@ -15,7 +15,7 @@ class CompanySettingsController < ApplicationController
       customer_support_email: @company_settings&.customer_support_email,
       customer_support_phone_number: @company_settings&.customer_support_phone_number,
       # logo_url: @company_settings&.logo&.attached? ? url_for(@company_settings.logo) : nil,
-      logo_url: @company_settings&.logo&.attached? ? rails_blob_url(@company_settings.logo, host: '102.221.35.116',
+      logo_url: @company_settings&.logo&.attached? ? rails_blob_url(@company_settings.logo, host: '38d6-102-221-35-116.ngrok-free.app',
       
       protocol: 'https'
       ) : nil
