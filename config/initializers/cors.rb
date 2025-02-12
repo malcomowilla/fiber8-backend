bc3047fb908f7fd71d5b1490b026530fecc20c46
malcomowilla <owillamalcom@gmail.com>
2025-02-12 20:27:26 +0300

    ip pool9

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,23 @@
 # config/initializers/cors.rb
 
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins 'http://localhost:5173'
+#       # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+#          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
+#       credentials: true  # Add this line if you're using cookies or session-based authentication
+
+#     end
+#   end
+
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5173'
-      # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
-         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-
-      credentials: true  # Add this line if you're using cookies or session-based authentication
-
-    end
+  allow do
+    origins ->(origin, _env) {
+      origin.present? && (origin == "https://aitechs.co.ke" || origin.match?(/^https:\/\/.*\.aitechs\.co\.ke$/))
+    }
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
   end
+end
 
