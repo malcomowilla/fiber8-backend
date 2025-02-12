bfe8f9444b4f3253bd5cbb087beddf65ed68c2d5
malcomowilla <owillamalcom@gmail.com>
2025-02-12 10:41:56 +0300

    postgres5

--- app/serializers/system_admin_serializer.rb
+++ app/serializers/system_admin_serializer.rb
@@ -1,7 +1,7 @@
 class SystemAdminSerializer < ActiveModel::Serializer
   attributes :id, :user_name, :email, :verification_token,
    :email_verified, :role, :fcm_token, :webauthn_id, 
-   :webauthn_authenticator_attachment, :login_with_passkey, :phone_number
+   :webauthn_authenticator_attachment, :login_with_passkey
 
 
 
