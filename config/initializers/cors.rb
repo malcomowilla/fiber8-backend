c5636e05aa38093c66d69de7898d8110318239f5
malcomowilla <owillamalcom@gmail.com>
2025-02-13 08:31:20 +0300

    fixing image rendering8

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
 
-      credentials: true  # Add this line if you're using cookies or session-based authentication
+      credentials: false  # Add this line if you're using cookies or session-based authentication
 
     end
   end
