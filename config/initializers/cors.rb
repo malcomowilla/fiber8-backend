25a45e5c0135a8683242377c36e8411aa975738e
malcomowilla <owillamalcom@gmail.com>
2025-02-13 05:37:07 +0300

    ip pool10

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ->(origin, _env) {
-      origin.present? && (origin == "https://aitechs.co.ke" || origin.match?(/^https:\/\/.*\.aitechs\.co\.ke$/))
+      origin.present? && (origin ==  origin.match?(/^https:\/\/.*\.aitechs\.co\.ke$/) || "https://aitechs.co.ke" == origin)
     }
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
   end
