fdd4f39c7a9dafa9ad9e9dc94ef4ef895a4ac25b
malcomowilla <owillamalcom@gmail.com>
2025-02-13 06:10:45 +0300

    ip pool17

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,26 +1,27 @@
 # config/initializers/cors.rb
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'https://fiber8.aitechs.co.ke'
-      # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
-         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-         expose: ['Content-Disposition']
-
-    end
-  end
-
-
-
-
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins ->(origin, _env) {
-#       origin.present? && (origin ==  origin.match?(/^https:\/\/.*\.aitechs\.co\.ke$/) || "https://aitechs.co.ke" == origin)
-#     }
-#     resource "*",
-#     expose: ['Content-Disposition'],
-#     headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+#     allow do
+#       origins 'http://localhost:5173'
+#       # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+#          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
+#       credentials: true  # Add this line if you're using cookies or session-based authentication
+
+#     end
 #   end
-# end
+
+
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ->(origin, _env) {
+      origin.present? && (origin ==  origin.match?(/^https:\/\/.*\.aitechs\.co\.ke$/) || "https://aitechs.co.ke" == origin)
+    }
+    resource "*",
+    expose: ['Content-Disposition'],
+    headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+  end
+end
 
