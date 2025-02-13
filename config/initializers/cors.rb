ac85ad40ddcd7cbc6cdfb42f4135ffe713c43a4b
malcomowilla <owillamalcom@gmail.com>
2025-02-13 06:02:03 +0300

    ip pool15

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'https://fiber8.aitechs.co.ke'
       # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-
-      credentials: true  # Add this line if you're using cookies or session-based authentication
+         expose: ['Content-Disposition'],
 
     end
   end
