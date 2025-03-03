5781e4854ccd78147bd428d60e5d1d9ef8a301fe
malcomowilla <owillamalcom@gmail.com>
2025-03-03 23:47:27 +0300

    jwt1

--- app/models/admin_setting.rb
+++ app/models/admin_setting.rb
@@ -1,7 +1,7 @@
 class AdminSetting < ApplicationRecord
   belongs_to :user
   acts_as_tenant(:account)
-
+  scope :for_user, ->(user_id) { where(user_id: user_id) }
   # before_validation :set_user, on: :create
 
   private
