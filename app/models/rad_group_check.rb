6c99db48181a0532102966113b453c4d5f9d3d58
malcomowilla <owillamalcom@gmail.com>
2025-03-25 18:00:55 +0300

    radius settings

--- app/models/rad_group_check.rb
+++ app/models/rad_group_check.rb
@@ -1,4 +1,12 @@
 class RadGroupCheck < ApplicationRecord
   self.table_name = 'radgroupcheck'  # Explicitly set the table name
 
+
+  # def attribute
+  #   self[:attribute]  # Access the database column directly
+  # end
+
+  # def attribute=(val)
+  #   self[:attribute] = val
+  # end
 end
