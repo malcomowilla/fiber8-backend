7a3ef7b8e3e8354aba650c97b9b6ddcb6d8c3cd2
malcomowilla <owillamalcom@gmail.com>
2025-03-31 14:50:52 +0300

    fetch url mikrotik

--- app/controllers/wireguard_controller.rb
+++ app/controllers/wireguard_controller.rb
@@ -30,7 +30,7 @@ class WireguardController < ApplicationController
   add allowed-address=0.0.0.0/0 endpoint-address=102.221.35.92 endpoint-port=51820 interface=wireguard1 persistent-keepalive=25s public-key="#{server_public_key}"
 
       /ip address
-      add address=#{client_ip}/24 interface=wireguard1
+      add address=#{client_ip} interface=wireguard1
     SCRIPT
 
     # Append new peer to wg0.conf (Ubuntu WireGuard server)
