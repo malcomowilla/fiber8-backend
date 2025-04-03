d9974e6db46fe18eabb5f60de33f7471c5790b15
malcomowilla <owillamalcom@gmail.com>
2025-04-03 10:04:34 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -129,6 +129,8 @@ get '/get_active_hotspot_users', to: 'hotspot_subscriptions#get_active_hotspot_u
 
 get '/api/get_active_hotspot_users', to: 'hotspot_subscriptions#get_active_hotspot_users'
 get '/allow_get_hotspot_templates ', to: 'hotspot_templates#allow_get_hotspot_templates'
+get '/api/allow_get_hotspot_templates ', to: 'hotspot_templates#allow_get_hotspot_templates'
+
 get '/api/router_info', to: 'router_info#router_info'
 post '/login_with_hotspot_voucher', to: 'hotspot_vouchers#login_with_hotspot_voucher'
 post '/api/login_with_hotspot_voucher', to: 'hotspot_vouchers#login_with_hotspot_voucher'
