fcc4bb1461d8736f4a1f0228d4094155003fbcf6
malcomowilla <owillamalcom@gmail.com>
2025-04-27 08:51:10 +0300

    errors

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
     acts_as_tenant(:account)
+    # validates :ppoe_username, presence: true
+    # validates :ppoe_password, presence: true
+
+    # validates :ppoe_username, uniqueness: true
 
 end
