e86b0ea31def7d2eeb20b5f5d50d95aec7c3c209
malcomowilla <owillamalcom@gmail.com>
2025-04-27 10:16:04 +0300

    errors

---
+++ app/sidekiq/radact_job.rb
@@ -0,0 +1,20 @@
+
+
+class RadactJob
+  include Sidekiq::Job
+  queue_as :default
+  
+  def perform
+    Account.find_each do |tenant|
+      ActsAsTenant.with_tenant(tenant) do
+    # Ensure ActsAsTenant is set for the current tenant, else set it manually if needed
+    # ActsAsTenant.current_tenant = Tenant.find(1)  # Or dynamically find the current tenant
+
+    # Update all RadAcct records where account_id is nil
+    RadAcct.where(account_id: nil).find_each do |radacct|
+      radacct.update(account_id: ActsAsTenant.current_tenant.id)
+    end
+  end
+      end
+      end
+end
