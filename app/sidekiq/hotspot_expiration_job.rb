12efafdfe1df28ad5d6816bff29325c716c594c2
malcomowilla <owillamalcom@gmail.com>
2025-04-27 13:06:07 +0300

    block users

--- app/sidekiq/hotspot_expiration_job.rb
+++ app/sidekiq/hotspot_expiration_job.rb
@@ -379,7 +379,54 @@ class HotspotExpirationJob
   def perform
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
+
+
+
+
+
+        expired_ppoe_subscriptions = Subscription.where("expiry <= ?", DateTime.current)
+
+        expired_ppoe_subscriptions.find_each do |subscription|
+          begin
+            router_setting = ActsAsTenant.current_tenant&.router_setting&.router_name
+    router = NasRouter.find_by(name: router_setting)
+
+    return unless router
+
+    router_ip = router.ip_address
+    router_username = router.username
+    router_password = router.password 
+            # SSH into MikroTik router
+            Net::SSH.start(router_ip, router_username , password: router_password) do |ssh|
+              # Add the user's IP address to the MikroTik Address List
+              ssh.exec!("ip firewall address-list add list=blocked_users address=#{subscription.ip_address} comment=#{subscription.ppoe_username}")
+        
+              puts "Blocked #{subscription.ppoe_username} (#{subscription.ip_address}) on MikroTik."
+        
+              # Update subscription status in DB
+              subscription.update!(status: 'blocked')
+            end
+          rescue => e
+            puts "Error blocking #{subscription.username}: #{e.message}"
+          end
+        end
+
+
+
+
+
+
+
+
+
+
         expired_vouchers = HotspotVoucher.where("expiration <= ? AND status != ?", Time.current, 'expired')
+
+
+
+
+
+
 
         expired_vouchers.each do |voucher|
           logout_hotspot_user(voucher)
