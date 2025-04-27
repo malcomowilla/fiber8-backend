ffc2af89b0147efde8bba293560ecc6517d6fa74
malcomowilla <owillamalcom@gmail.com>
2025-04-27 09:23:52 +0300

    errors

--- db/migrate/20250427062159_add_account_id_integer_to_rad_acct.rb
+++ db/migrate/20250427062159_add_account_id_integer_to_rad_acct.rb
@@ -1,5 +1,5 @@
 class AddAccountIdIntegerToRadAcct < ActiveRecord::Migration[7.2]
   def change
-    add_column :rad_acct, :account_id, :integer
+    add_column :radacct, :account_id, :integer
   end
 end
