803c6cb37f67eac0a3a8f2843b27da33573c76a5
malcomowilla <owillamalcom@gmail.com>
2025-04-27 10:09:03 +0300

    errors

--- app/models/rad_acct.rb
+++ app/models/rad_acct.rb
@@ -1,8 +1,15 @@
 class RadAcct < ApplicationRecord
-  self.table_name = 'radacct'  # Explicitly set the table name
-  # acts_as_tenant(:account)
+  self.table_name = 'radacct'
+  acts_as_tenant(:account)
+
   self.ignored_columns = ["class"]
 
+  # Set the account_id for records without it
+  def self.set_account_id_for_missing_account
+    RadAcct.where(account_id: nil).find_each do |radacct|
+      radacct.update(account_id: ActsAsTenant.current_tenant.id)
+    end
+  end
 end
 
 
