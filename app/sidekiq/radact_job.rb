b04b2882edbce3ae93801c0d929af02d4c71ace0
malcomowilla <owillamalcom@gmail.com>
2025-04-27 11:17:21 +0300

    errors

--- app/sidekiq/radact_job.rb
+++ app/sidekiq/radact_job.rb
@@ -9,17 +9,19 @@ class RadactJob
       ActsAsTenant.with_tenant(tenant) do
     # Ensure ActsAsTenant is set for the current tenant, else set it manually if needed
     # ActsAsTenant.current_tenant = Tenant.find(1)  # Or dynamically find the current tenant
-    nil_radacct_count = RadAcct.where(account_id: nil).count
+    nil_radacct_count = RadAcct.unscoped.where(account_id: nil).count
     Rails.logger.info "Found #{nil_radacct_count} RadAcct records with nil account_id for tenant #{ActsAsTenant.current_tenant.id}"
     
     # Update all RadAcct records where account_id is nil
-    RadAcct.where(account_id: nil).find_each do |radacct|
+    #         RadAcct.unscoped.where(account_id: nil).find_each do |radacct|
+    RadAcct.unscoped.where(account_id: nil).find_each do |radacct|
+
       # Rails.logger.info "radct update in job#{radacct}"
       # radacct.update!(account_id: ActsAsTenant.current_tenant.id)
       # RadAcct.where(account_id: nil).find_each do |radacct|
   if radacct
     begin
-      # radacct.update!(account_id: ActsAsTenant.current_tenant.id)
+      radacct.update!(account_id: ActsAsTenant.current_tenant.id)
     rescue => e
       Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
     end
