63a3f03b9964b74b54a1ccf77c4290fe235fd9e3
malcomowilla <owillamalcom@gmail.com>
2025-04-27 10:00:09 +0300

    errors

--- app/models/rad_acct.rb
+++ app/models/rad_acct.rb
@@ -1,6 +1,6 @@
 class RadAcct < ApplicationRecord
   self.table_name = 'radacct'  # Explicitly set the table name
-  acts_as_tenant(:account)
+  # acts_as_tenant(:account)
   self.ignored_columns = ["class"]
 
 end
