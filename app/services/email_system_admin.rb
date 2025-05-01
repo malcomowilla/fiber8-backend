41b2735408ea71aaa1c95c1c211a426304caea5d
malcomowilla <owillamalcom@gmail.com>
2025-05-01 11:50:28 +0300

    email

--- app/services/email_system_admin.rb
+++ app/services/email_system_admin.rb
@@ -7,10 +7,22 @@ class EmailSystemAdmin
       Rails.logger.warn "No current account found for system admin. Using fallback email settings."
       set_fallback_settings
     elsif current_system_admin.present?
-      Rails.application.config.action_mailer.delivery_method = :mailtrap
-      Rails.application.config.action_mailer.mailtrap_settings = {
-        api_key: "d848f326f33a7aa8db359e399fd7c510"
-      }
+      # Rails.application.config.action_mailer.delivery_method = :mailtrap
+      # Rails.application.config.action_mailer.mailtrap_settings = {
+      #   api_key: "d848f326f33a7aa8db359e399fd7c510"
+      # }
+      #  Rails.application.config.action_mailer.delivery_method = :smtp
+Rails.application.config.action_mailer.smtp_settings = {
+  user_name: email_setting.smtp_username,
+  password: email_setting.smtp_password,
+  address: email_setting.smtp_host,
+  domain: 'aitechs.co.ke',
+  port: email_setting.smtp_port,
+  authentication: :login,
+  ssl: true,
+  tls: true,
+  enable_starttls_auto: true
+}
       # configure_for_account(current_account, current_system_admin)
     end
   rescue StandardError => e 
@@ -25,20 +37,45 @@ class EmailSystemAdmin
 
     if email_setting&.api_key.present?
       Rails.logger.info "Configuring Mailtrap with API Key"
-      Rails.application.config.action_mailer.delivery_method = :mailtrap
-      Rails.application.config.action_mailer.mailtrap_settings = {
-        api_key: email_setting.api_key
-      }
-    elsif email_setting.present?
-      Rails.logger.info "Configuring SMTP with Email Settings"
+      # Rails.application.config.action_mailer.delivery_method = :mailtrap
+      # Rails.application.config.action_mailer.mailtrap_settings = {
+      #   api_key: email_setting.api_key
+      # }
+      
       Rails.application.config.action_mailer.delivery_method = :smtp
       Rails.application.config.action_mailer.smtp_settings = {
         user_name: email_setting.smtp_username,
         password: email_setting.smtp_password,
         address: email_setting.smtp_host,
-        domain: email_setting.domain,
-        port: '587',
-        authentication: :plain,
+        domain: 'aitechs.co.ke',
+        port: email_setting.smtp_port,
+        authentication: :login,
+        ssl: true,
+        tls: true,
+        enable_starttls_auto: true
+      }
+    elsif email_setting.present?
+      Rails.logger.info "Configuring SMTP with Email Settings"
+      # Rails.application.config.action_mailer.delivery_method = :smtp
+      # Rails.application.config.action_mailer.smtp_settings = {
+      #   user_name: email_setting.smtp_username,
+      #   password: email_setting.smtp_password,
+      #   address: email_setting.smtp_host,
+      #   domain: email_setting.domain,
+      #   port: '587',
+      #   authentication: :plain,
+      #   enable_starttls_auto: true
+      # }
+      Rails.application.config.action_mailer.delivery_method = :smtp
+      Rails.application.config.action_mailer.smtp_settings = {
+        user_name: email_setting.smtp_username,
+        password: email_setting.smtp_password,
+        address: email_setting.smtp_host,
+        domain: 'aitechs.co.ke',
+        port: email_setting.smtp_port,
+        authentication: :login,
+        ssl: true,
+        tls: true,
         enable_starttls_auto: true
       }
     elsif current_system_admin.present?
@@ -50,8 +87,20 @@ class EmailSystemAdmin
 
   def self.set_fallback_settings
     Rails.application.config.action_mailer.delivery_method = :mailtrap
-    Rails.application.config.action_mailer.mailtrap_settings = {
-      api_key: "d848f326f33a7aa8db359e399fd7c510"
+    # Rails.application.config.action_mailer.mailtrap_settings = {
+    #   api_key: "d848f326f33a7aa8db359e399fd7c510"
+    # }
+    Rails.application.config.action_mailer.delivery_method = :smtp
+    Rails.application.config.action_mailer.smtp_settings = {
+      user_name: email_setting.smtp_username,
+      password: email_setting.smtp_password,
+      address: email_setting.smtp_host,
+      domain: 'aitechs.co.ke',
+      port: email_setting.smtp_port,
+      authentication: :login,
+      ssl: true,
+      tls: true,
+      enable_starttls_auto: true
     }
   end
 end
