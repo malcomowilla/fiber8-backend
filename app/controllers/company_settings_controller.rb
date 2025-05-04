1bfa8baac401500f5f2725ec7814875c3de7d49c
malcomowilla <owillamalcom@gmail.com>
2025-05-04 17:26:16 +0300

    cloudfared

--- app/controllers/company_settings_controller.rb
+++ app/controllers/company_settings_controller.rb
@@ -155,7 +155,7 @@ logo_url: @company_settings&.logo&.attached? ? rails_blob_url(@company_settings.
 
 
     match = log_output.match(%r{https://([a-z0-9-]+\.trycloudflare\.com)})
-    match[1] if match
+    match[1] 
     Rails.logger.info "Cloudflare tunnel hostname: #{match[1]}"
   end
   
