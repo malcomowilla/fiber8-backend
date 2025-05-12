5fa466d9e3a88e427ae1cbf4ed3836cd374cf337
malcomowilla <owillamalcom@gmail.com>
2025-05-12 11:29:48 +0300

    sidekiq

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -13,12 +13,13 @@ load_and_authorize_resource
   
     
 def set_current_tenant
-
   host = request.headers['X-Subdomain']
   @account = Account.find_by(subdomain: host)
-   @current_account=ActsAsTenant.current_tenant 
+  @current_account= ActsAsTenant.current_tenant 
   EmailConfiguration.configure(@current_account, ENV['SYSTEM_ADMIN_EMAIL'])
+  # EmailSystemAdmin.configure(@current_account, current_system_admin)
 
+  Rails.logger.info "set_current_tenant #{ActsAsTenant.current_tenant.inspect}"
   # set_current_tenant(@account)
 rescue ActiveRecord::RecordNotFound
   render json: { error: 'Invalid tenant' }, status: :not_found
