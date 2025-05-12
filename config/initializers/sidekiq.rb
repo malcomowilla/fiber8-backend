cc0ffb4dca4761aba12125db382f9f837ad2163c
malcomowilla <owillamalcom@gmail.com>
2025-05-12 08:18:01 +0300

    -sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,21 +1,4 @@
-# # config/initializers/sidekiq.rb
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://localhost:6379/0' }
-#     config.on(:startup) do
-#       Sidekiq.schedule = {
-        
-#         'router_ping_job' => {
-#         'class' => 'RouterPingJob',
-#         'cron' => '* * * * *' # Run every minute
-#       }
-#       }
-#       Sidekiq::Scheduler.reload_schedule!
-#     end
-#   end
-  
-
-
-
+# config/initializers/sidekiq.rb
 require 'sidekiq'
 require 'sidekiq-scheduler'
 
@@ -24,42 +7,25 @@ Sidekiq.configure_server do |config|
 
   config.on(:startup) do
     schedule = {
-      
-  'subscription_expired_job' => {
-          'class' => 'SubscriptionExpiredJob',
-          'cron' => '* * * * *', # Every minute
-          'queue' => 'default',
-          'description' => 'Block expired PPPoE subscriptions'
-        },
-
-
+      'subscription_expired_job' => {
+        'class' => 'SubscriptionExpiredJob',
+        'cron' => '* * * * *', # Every minute
+        'queue' => 'default'
+      },
       'router_ping_job' => {
-  'class' => 'RouterPingJob',
-  'cron' => '* * * * *' # Run every 4 minutes
-},
-
-'system_metrics_job' => {
-  'class' => 'SystemMetricsJob',
-  'cron' => '* * * * *' # Run every minute
-},
-
-  
-  'hotspot_expiration_job' => {
-    'class' => 'HotspotExpirationJob',
-    'cron' => '* * * * *'
-  },
-
-
-   
-
-
-     
-      
+        'class' => 'RouterPingJob',
+        'cron' => '* * * * *',
+        'queue' => 'default'
+      },
+      'hotspot_expiration_job' => {
+        'class' => 'HotspotExpirationJob',
+        'cron' => '*/4 * * * *',
+        'queue' => 'default'
+      }
     }
 
     Sidekiq.schedule = schedule
     Sidekiq::Scheduler.reload_schedule!
-    Rails.logger.info("Sidekiq schedule loaded: #{Sidekiq.schedule.inspect}")
-
+    Rails.logger.info "Loaded Sidekiq schedule: #{Sidekiq.schedule.keys}"
   end
 end
