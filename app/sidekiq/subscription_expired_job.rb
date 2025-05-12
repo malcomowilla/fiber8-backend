a3291be21889a05603955213f0116f33dfdcbc49
malcomowilla <owillamalcom@gmail.com>
2025-05-12 08:02:45 +0300

    -sidekiq

--- app/sidekiq/subscription_expired_job.rb
+++ app/sidekiq/subscription_expired_job.rb
@@ -49,34 +49,91 @@
 
 #   end
 # app/jobs/subscription_expired_job.rb
+ 
+
+
+
+# class SubscriptionExpiredJob
+#   include Sidekiq::Worker
+
+#   def perform
+#     Account.find_each do |tenant|
+#       ActsAsTenant.with_tenant(tenant) do
+#         expired_ppoe_subscriptions = Subscription.where("expiry <= ?", DateTime.current)
+
+#         expired_ppoe_subscriptions.find_each do |subscription|
+#           begin
+#             router_setting = ActsAsTenant.current_tenant&.router_setting&.router_name
+#             router = NasRouter.find_by(name: router_setting)
+#             next unless router
+
+#             Net::SSH.start(
+#               router.ip_address,
+#               router.username,
+#               password: router.password
+#             ) do |ssh|
+#               ssh.exec!("ip firewall address-list add list=aitechs_blocked_list address=#{subscription.ip_address} comment=#{subscription.ppoe_username}")
+#               Rails.logger.info("Blocked #{subscription.ppoe_username} (#{subscription.ip_address}) on MikroTik.")
+#               subscription.update!(status: 'blocked')
+#             end
+#           rescue => e
+#             Rails.logger.error("Error blocking #{subscription.ppoe_username}: #{e.message}")
+#           end
+#         end
+#       end
+#     end
+#   end
+# end
+
+
+
+
+
+# app/jobs/subscription_expired_job.rb
 class SubscriptionExpiredJob
   include Sidekiq::Worker
+  sidekiq_options queue: 'default'
 
   def perform
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
-        expired_ppoe_subscriptions = Subscription.where("expiry <= ?", DateTime.current)
-
-        expired_ppoe_subscriptions.find_each do |subscription|
-          begin
-            router_setting = ActsAsTenant.current_tenant&.router_setting&.router_name
-            router = NasRouter.find_by(name: router_setting)
-            next unless router
-
-            Net::SSH.start(
-              router.ip_address,
-              router.username,
-              password: router.password
-            ) do |ssh|
-              ssh.exec!("ip firewall address-list add list=aitechs_blocked_list address=#{subscription.ip_address} comment=#{subscription.ppoe_username}")
-              Rails.logger.info("Blocked #{subscription.ppoe_username} (#{subscription.ip_address}) on MikroTik.")
-              subscription.update!(status: 'blocked')
-            end
-          rescue => e
-            Rails.logger.error("Error blocking #{subscription.ppoe_username}: #{e.message}")
-          end
-        end
+        process_expired_subscriptions
       end
     end
   end
+
+  private
+
+  def process_expired_subscriptions
+    Subscription.where("expiry <= ?", DateTime.current).find_each do |subscription|
+      block_subscription(subscription)
+    rescue => e
+      Rails.logger.error("Error blocking #{subscription.ppoe_username}: #{e.message}")
+    end
+  end
+
+  def block_subscription(subscription)
+    router = NasRouter.find_by(
+      account_id: ActsAsTenant.current_tenant.id,
+      name: ActsAsTenant.current_tenant.router_setting&.router_name
+    )
+    return unless router
+
+    Net::SSH.start(
+      router.ip_address,
+      router.username,
+      password: router.password,
+      timeout: 10 # Add timeout
+    ) do |ssh|
+      ssh.exec!("ip firewall address-list add list=aitechs_blocked_list address=#{subscription.ip_address} comment=#{subscription.ppoe_username}")
+      subscription.update!(status: 'blocked')
+      Rails.logger.info("Blocked #{subscription.ppoe_username} (#{subscription.ip_address})")
+    end
+  end
 end
+
+
+
+
+
+
