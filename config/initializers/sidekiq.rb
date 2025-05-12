7824e1afd265f41e526358d7c4542221f63a5597
malcomowilla <owillamalcom@gmail.com>
2025-05-12 06:58:59 +0300

    -sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,61 +1,61 @@
-# # config/initializers/sidekiq.rb
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://localhost:6379/0' }
-#     config.on(:startup) do
-#       Sidekiq.schedule = {
+# # # config/initializers/sidekiq.rb
+# # Sidekiq.configure_server do |config|
+# #   config.redis = { url: 'redis://localhost:6379/0' }
+# #     config.on(:startup) do
+# #       Sidekiq.schedule = {
         
-#         'router_ping_job' => {
-#         'class' => 'RouterPingJob',
-#         'cron' => '* * * * *' # Run every minute
-#       }
-#       }
-#       Sidekiq::Scheduler.reload_schedule!
-#     end
-#   end
+# #         'router_ping_job' => {
+# #         'class' => 'RouterPingJob',
+# #         'cron' => '* * * * *' # Run every minute
+# #       }
+# #       }
+# #       Sidekiq::Scheduler.reload_schedule!
+# #     end
+# #   end
   
 
 
 
-require 'sidekiq'
-require 'sidekiq-scheduler'
+# require 'sidekiq'
+# require 'sidekiq-scheduler'
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/0' }
 
-  config.on(:startup) do
-    schedule = {
-      # 'router_ping_job' => {
-      #   'class' => 'RouterPingJob',
-      #   'cron' => '* * * * *' # Run every minute
-      # },
+#   config.on(:startup) do
+#     schedule = {
+#       # 'router_ping_job' => {
+#       #   'class' => 'RouterPingJob',
+#       #   'cron' => '* * * * *' # Run every minute
+#       # },
 
-      # 'radact_job' => {
-      #   'class' => 'RadactJob',
-      #   'cron' => '* * * * *' # Run every minute
+#       # 'radact_job' => {
+#       #   'class' => 'RadactJob',
+#       #   'cron' => '* * * * *' # Run every minute
 
-      # },
+#       # },
 
-      # 'system_metrics_job' => {
-      #   'class' => 'SystemMetricsJob',
-      #   'cron' => '* * * * *' # Run every minute
-      # },
-
-
+#       # 'system_metrics_job' => {
+#       #   'class' => 'SystemMetricsJob',
+#       #   'cron' => '* * * * *' # Run every minute
+#       # },
 
 
-      # 'hotspot_expiration_job' => {
-      #   'class' => 'HotspotExpirationJob',
-      #   'cron' => '* * * * *' # Run every minute
-      # },
+
+
+#       # 'hotspot_expiration_job' => {
+#       #   'class' => 'HotspotExpirationJob',
+#       #   'cron' => '* * * * *' # Run every minute
+#       # },
       
-#       'hotspot_expiration_job' => {
-#   'class' => 'HotspotExpirationJob',
-#   'cron' => '*/4 * * * *' # Run every 4 minutes
-# }
+# #       'hotspot_expiration_job' => {
+# #   'class' => 'HotspotExpirationJob',
+# #   'cron' => '*/4 * * * *' # Run every 4 minutes
+# # }
       
-    }
+#     }
 
-    Sidekiq.schedule = schedule
-    Sidekiq::Scheduler.reload_schedule!
-  end
-end
+#     Sidekiq.schedule = schedule
+#     Sidekiq::Scheduler.reload_schedule!
+#   end
+# end
