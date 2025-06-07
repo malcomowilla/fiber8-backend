05678c090c1755c1e056ddad6063429d82c83939
malcomowilla <owillamalcom@gmail.com>
2025-06-07 18:04:03 +0300

    push not

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,7 +2,7 @@ class TestJob < ApplicationJob
   queue_as :default
   def perform(*args)
     Rails.logger.info "🔥🔥🔥 TestJob is running with args: #{args.inspect}"
-    User.update_all(status: 'testing')
+    # User.update_all(status: 'testing')
     puts "🔥🔥🔥 TestJob is running with args: #{args.inspect}"
   end
 end
