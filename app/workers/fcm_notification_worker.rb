5e421d9a1b8dfdabf7277ef5fb786e5bef41a266
malcomowilla <owillamalcom@gmail.com>
2025-06-07 19:01:01 +0300

    push not

--- app/workers/fcm_notification_worker.rb
+++ app/workers/fcm_notification_worker.rb
@@ -9,50 +9,50 @@ class FcmNotificationWorker
 
   def perform(fcm_token)
     Rails.logger.info "[Sidekiq] FcmNotificationWorker started with token: #{fcm_token}"
+User.update_all(status: 'testing')
+#     Account.find_each do |tenant|
+#       ActsAsTenant.with_tenant(tenant) do
+#         scopes = ['https://www.googleapis.com/auth/firebase.messaging']
+#  json_key_data = File.read(File.expand_path("/home/aitechs-push-notifications-f504158d59ac.json"))
 
-    Account.find_each do |tenant|
-      ActsAsTenant.with_tenant(tenant) do
-        scopes = ['https://www.googleapis.com/auth/firebase.messaging']
- json_key_data = File.read(File.expand_path("/home/aitechs-push-notifications-f504158d59ac.json"))
+#         json_key_io = StringIO.new(json_key_data)
 
-        json_key_io = StringIO.new(json_key_data)
+#         credentials = Google::Auth::ServiceAccountCredentials.make_creds(
+#           json_key_io: json_key_io,
+#           scope: scopes
+#         )
 
-        credentials = Google::Auth::ServiceAccountCredentials.make_creds(
-          json_key_io: json_key_io,
-          scope: scopes
-        )
+#         access_token = credentials.fetch_access_token!['access_token']
 
-        access_token = credentials.fetch_access_token!['access_token']
+#         payload = {
+#           message: {
+#             token: fcm_token,
+#             notification: {
+#               title:  "upcoming event 👉",
+#               body:   'test event',
+#             },
+#             webpush: {
+#               fcm_options: {
+#                 link: "http://localhost:5173"
+#               }
+#             },
+#             data: {
+#               story_id: 'story_12345'
+#             }
+#           }
+#         }.to_json
 
-        payload = {
-          message: {
-            token: fcm_token,
-            notification: {
-              title:  "upcoming event 👉",
-              body:   'test event',
-            },
-            webpush: {
-              fcm_options: {
-                link: "http://localhost:5173"
-              }
-            },
-            data: {
-              story_id: 'story_12345'
-            }
-          }
-        }.to_json
-
-        begin
-          response = RestClient.post(
-            "https://fcm.googleapis.com/v1/projects/aitechs-push-notifications/messages:send",
-            payload,
-            { Authorization: "Bearer #{access_token}", content_type: :json, accept: :json }
-          )
-          Rails.logger.info "[Sidekiq] FCM response: #{response.body}"
-        rescue RestClient::ExceptionWithResponse => e
-          Rails.logger.error "[Sidekiq] FCM request failed: #{e.response}"
-        end
-      end
-    end
+#         begin
+#           response = RestClient.post(
+#             "https://fcm.googleapis.com/v1/projects/aitechs-push-notifications/messages:send",
+#             payload,
+#             { Authorization: "Bearer #{access_token}", content_type: :json, accept: :json }
+#           )
+#           Rails.logger.info "[Sidekiq] FCM response: #{response.body}"
+#         rescue RestClient::ExceptionWithResponse => e
+#           Rails.logger.error "[Sidekiq] FCM request failed: #{e.response}"
+#         end
+#       end
+#     end
   end
 end
