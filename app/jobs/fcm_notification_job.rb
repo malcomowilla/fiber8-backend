ed3830be09098c25f2b6bbc791e3e264c4f29b23
malcomowilla <owillamalcom@gmail.com>
2025-06-07 19:25:55 +0300

    push not

--- app/jobs/fcm_notification_job.rb
+++ app/jobs/fcm_notification_job.rb
@@ -7,14 +7,13 @@ require 'json'
 
 
 
-  def perform(fcm_token)
-    Rails.logger.info "FcmNotificationJob started"
+  def perform(event_id, fcm_token)
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
 
-    # @calendar_event = CalendarEvent.find_by(id: event_id)
-    # scopes = ['https://www.googleapis.com/auth/firebase.messaging']
+    @calendar_event = CalendarEvent.find_by(id: event_id)
     scopes = ['https://www.googleapis.com/auth/firebase.messaging']
+    # scopes = ['https://www.googleapis.com/auth/firebase.messaging']
  
 #  json_key_data = File.read(File.expand_path("~/Downloads/aitechs-push-notifications-f504158d59ac.json"))
  json_key_data = File.read(File.expand_path("/home/aitechs-push-notifications-f504158d59ac.json"))
@@ -30,45 +29,45 @@ json_key_io = StringIO.new(json_key_data)
       json_key_io: json_key_io,
       scope: scopes
     )
-    # event_start_time = @calendar_event.start.in_time_zone
-    # current_time = Time.current.in_time_zone
+    event_start_time = @calendar_event.start.in_time_zone
+    current_time = Time.current.in_time_zone
     
-    # Rails.logger.info "#{event_start_time}, Current time: #{current_time}"
+    Rails.logger.info "#{event_start_time}, Current time: #{current_time}"
     
-    # if event_start_time < current_time
-    #   Rails.logger.info "Event start time has already passed. Skipping notification."
-    # else
-    #   Rails.logger.info "Event is in the future. Proceeding with notification."
-    # end
+    if event_start_time < current_time
+      Rails.logger.info "Event start time has already passed. Skipping notification."
+    else
+      Rails.logger.info "Event is in the future. Proceeding with notification."
+    end
     
     # Fetch access token
     access_token = credentials.fetch_access_token!['access_token']
     
-    # calendar_settings = ActsAsTenant.current_tenant.calendar_setting
-    # in_minutes = calendar_settings.start_in_minutes 
-    # in_hours = calendar_settings.start_in_hours
+    calendar_settings = ActsAsTenant.current_tenant.calendar_setting
+    in_minutes = calendar_settings.start_in_minutes 
+    in_hours = calendar_settings.start_in_hours
 
 
     body_message = []
     
-# if in_minutes.to_i > 0
-#   body_message << "The event is starting in #{in_minutes} minutes at 👉" + @calendar_event.start.strftime('%I:%M %p')
-# end
+if in_minutes.to_i > 0
+  body_message << "The event is starting in #{in_minutes} minutes at 👉" + @calendar_event.start.strftime('%I:%M %p')
+end
 
-# if in_hours.to_i > 0
-#   body_message << "The event is starting in #{in_hours} hours at 👉" + @calendar_event.start.strftime('%I:%M %p')
-# end
+if in_hours.to_i > 0
+  body_message << "The event is starting in #{in_hours} hours at 👉" + @calendar_event.start.strftime('%I:%M %p')
+end
     # FcmNotificationJob.perform_now(@calendar_event.id, @fcm_token)
     
-    # formatted_body = body_message.join("\n")
+    formatted_body = body_message.join("\n")
 
     # Define the request payload (sending to a specific device)
     payload = {
       message: {
         token: fcm_token, 
         notification: {
-          title:  "upcoming event 👉" ,
-          body:  'test event', 
+          title:  "upcoming event 👉" +  @calendar_event.title,
+          body:  formatted_body, 
         
           
           # image: 'https://quality-smiles-bucket.s3.eu-north-1.
