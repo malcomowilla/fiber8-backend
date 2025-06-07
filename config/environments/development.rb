e9a28153603a8214b56e61c7a78a6bac162234d2
malcomowilla <owillamalcom@gmail.com>
2025-06-07 18:53:55 +0300

    push not

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.enable_reloading = true
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.active_job.queue_adapter = :sidekiq
+  # config.active_job.queue_adapter = :sidekiq
 
 
 
