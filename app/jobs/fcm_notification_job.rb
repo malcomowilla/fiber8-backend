b52cfa556989ff25e052732f7598f386e92bb140
malcomowilla <owillamalcom@gmail.com>
2025-06-07 18:40:14 +0300

    push not

--- app/jobs/fcm_notification_job.rb
+++ app/jobs/fcm_notification_job.rb
@@ -17,7 +17,7 @@ require 'json'
     scopes = ['https://www.googleapis.com/auth/firebase.messaging']
  
 #  json_key_data = File.read(File.expand_path("~/Downloads/aitechs-push-notifications-f504158d59ac.json"))
- json_key_data = File.read(File.expand_path("~/home/aitechs-push-notifications-f504158d59ac.json"))
+ json_key_data = File.read(File.expand_path("/home/aitechs-push-notifications-f504158d59ac.json"))
 
  
 json_key_io = StringIO.new(json_key_data)
