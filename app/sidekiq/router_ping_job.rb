8b4bce996358e6171a0f2994e4c71fbfb7764c8d
malcomowilla <owillamalcom@gmail.com>
2025-06-11 14:59:29 +0300

    sidekiq jobs

--- app/sidekiq/router_ping_job.rb
+++ app/sidekiq/router_ping_job.rb
@@ -6,25 +6,8 @@ class RouterPingJob
 
   def perform
 
-log_output = `journalctl -u cloudflared -n 100 --no-pager --reverse`
 
-
-
-
-
-
-    unless log_output.match?(%r{https://[a-z0-9-]+\.trycloudflare\.com})
-      Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
-
-      # Restart cloudflared and your backend service
-      `systemctl restart cloudflared`
-      `systemctl restart aitechs-fiber8-backend`
-
-      Rails.logger.info "[Tunnel Monitor] Services restarted successfully."
-    else
-      Rails.logger.info "[Tunnel Monitor] Tunnel is active. No action needed."
-    end
-
+    
     Account.find_each do |tenant| # Iterate over all tenants
       ActsAsTenant.with_tenant(tenant) do
 
