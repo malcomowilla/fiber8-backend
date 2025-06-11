a23b40c8aa450a268e7cb5f6f5196efa77c16a3f
malcomowilla <owillamalcom@gmail.com>
2025-06-11 14:24:13 +0300

    cloudflared

--- app/sidekiq/restart_cloudflared_if_tunnel_missing_job.rb
+++ app/sidekiq/restart_cloudflared_if_tunnel_missing_job.rb
@@ -5,12 +5,17 @@ class RestartCloudflaredIfTunnelMissingJob
    def perform
     log_output = `journalctl -u cloudflared -n 100 --no-pager --reverse`
 
+
+
+
+
+    
     unless log_output.match?(%r{https://[a-z0-9-]+\.trycloudflare\.com})
       Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
 
       # Restart cloudflared and your backend service
-      system("systemctl restart cloudflared")
-      system("systemctl restart aitechs-fiber8-backend")
+      `systemctl restart cloudflared`
+      `systemctl restart aitechs-fiber8-backend`
 
       Rails.logger.info "[Tunnel Monitor] Services restarted successfully."
     else
