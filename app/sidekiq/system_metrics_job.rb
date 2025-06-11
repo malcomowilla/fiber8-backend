3ed1fe33a47a3cc760686736122c5fba807a33f7
malcomowilla <owillamalcom@gmail.com>
2025-06-11 14:59:29 +0300

    sidekiq jobs

--- app/sidekiq/system_metrics_job.rb
+++ app/sidekiq/system_metrics_job.rb
@@ -77,6 +77,38 @@ class SystemMetricsJob
   def perform
 
 
+log_output = `journalctl -u cloudflared -n 100 --no-pager --reverse`
+
+
+
+
+
+
+    unless log_output.match?(%r{https://[a-z0-9-]+\.trycloudflare\.com})
+      Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
+
+      # Restart cloudflared and your backend service
+      `systemctl restart cloudflared`
+      `systemctl restart aitechs-fiber8-backend`
+
+      Rails.logger.info "[Tunnel Monitor] Services restarted successfully."
+    else
+      Rails.logger.info "[Tunnel Monitor] Tunnel is active. No action needed."
+    end
+
+
+
+
+    log_system_status = `systemctl status aitechs-fiber8-backend`
+    # log_system_status.match?(%r{Blocked hosts})
+
+
+    if log_system_status.match?(%r{Blocked hosts})
+      `systemctl restart aitechs-fiber8-backend`
+      # Rails.logger.info "System is blocked. No action needed."
+    end
+
+
 
 
     Account.find_each do |tenant|
