d8af0b46d093785ba99f32cb8060560437d8602f
malcomowilla <owillamalcom@gmail.com>
2025-06-18 19:12:53 +0300

    private ips

--- app/sidekiq/rehydrate_wireguard_job.rb
+++ app/sidekiq/rehydrate_wireguard_job.rb
@@ -7,9 +7,10 @@ class RehydrateWireguardJob
     Rails.logger.info "Rehydrating wireguard"
     WireguardPeer.find_each do |peer|
       # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
-      `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},192.168.50.47/32`
+      # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},192.168.50.47/32`
+      `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},#{peer.private_ip}`
 
-      
+
     end
   end
 end
