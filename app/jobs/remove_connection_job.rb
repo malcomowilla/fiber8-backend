b7dc664d456aa5409b9ce3dd43968a526e850eec
malcomowilla <owillamalcom@gmail.com>
2025-07-17 11:21:47 +0300

    rails job

---
+++ app/jobs/remove_connection_job.rb
@@ -0,0 +1,48 @@
+
+
+class RemoveConnectionJob < ApplicationJob
+  queue_as :default
+require 'net/ssh'
+require 'ipaddr'
+require 'open3'
+
+  def perform(ppoe_username, subscription)
+  begin
+        
+     nas = IpNetwork.find_by(title: subscription.network_name).nas
+
+     
+        router = NasRouter.find_by(name: nas)
+      
+        return unless router
+        
+        router_ip = router.ip_address
+        router_username = router.username
+        router_password = router.password 
+        
+        # Connect via SSH to MikroTik
+        Net::SSH.start(router_ip, router_username, password: router_password, verify_host_key: :never, non_interactive: true) do |ssh|
+          # Correct command to remove active PPPoE session based on pppoe_username
+          command = "/ppp active remove [find name=#{ppoe_username}]"
+          
+          # Execute the command
+          ssh.exec!(command)
+        end
+      rescue StandardError => e
+        Rails.logger.error "Error removing PPPoE connection for username #{ppoe_username}: #{e.message}"
+      end
+  
+
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
