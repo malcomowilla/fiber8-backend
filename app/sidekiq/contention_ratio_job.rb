8c141f6a5bd6a8ed2273aae3aa36c78474c086ad
malcomowilla <owillamalcom@gmail.com>
2025-07-17 23:50:12 +0300

    free radius

--- app/sidekiq/contention_ratio_job.rb
+++ app/sidekiq/contention_ratio_job.rb
@@ -10,7 +10,7 @@ class ContentionRatioJob
         router_username = router.username
         router_password = router.password
 
-        Net::SSH.start(router_ip, router_username, password: router_password, verify_host_key: :never, non_interactive: true) do |ssh|
+        Net::SSH.start(router_ip, router_username, password: router_password, verify_host_key: :never) do |ssh|
 
           # Fetch active PPPoE users
           active_users_raw = ssh.exec!('/ppp active print detail without-paging')
