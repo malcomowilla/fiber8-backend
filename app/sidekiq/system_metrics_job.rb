b03f5b063cc8bed88f8943d9a73430ca079d50c0
malcomowilla <owillamalcom@gmail.com>
2025-07-18 20:19:22 +0300

    free radius

--- app/sidekiq/system_metrics_job.rb
+++ app/sidekiq/system_metrics_job.rb
@@ -78,102 +78,6 @@ class SystemMetricsJob
 
 
 
-NasRouter.all.each do |router|
-      begin
-        router_ip       = router.ip_address
-        router_username = router.username
-        router_password = router.password
-
-        # Step 1: Fetch active PPPoE users
-        active_users = fetch_active_users(router_ip, router_username, router_password)
-        next if active_users.blank?
-
-        active_users.each do |user|
-          pppoe_username = user['name']
-          subscription   = Subscription.find_by(ppoe_username: pppoe_username)
-          next unless subscription
-
-          package = Package.find_by(name: subscription.package_name)
-          next unless package&.aggregation.present?
-
-          aggregation_ratio = package.aggregation.to_i
-          download_limit    = package.download_limit.to_f
-          upload_limit      = package.upload_limit.to_f
-
-          shared_download = (download_limit / aggregation_ratio).round(2)
-          shared_upload   = (upload_limit / aggregation_ratio).round(2)
-
-          queue_name = "queue_#{pppoe_username}"
-          target_ip  = subscription.ip_address
-          next if target_ip.blank?
-
-          # Step 2: Check if queue already exists
-          if queue_exists?(router_ip, router_username, router_password, queue_name)
-            Rails.logger.info "[ContentionRatioJob] Queue exists for #{queue_name}, skipping."
-            next
-          end
-
-          # Step 3: Add queue
-          payload = {
-            name: queue_name,
-            target: target_ip,
-            "max-limit": "#{shared_upload}M/#{shared_download}M",
-            # "burst-threshold": "#{package.burst_threshold_upload}/#{package.burst_threshold_download}",
-            "burst-limit": "#{package.burst_upload_speed}M/#{package.burst_download_speed}M",
-            
-              "burst-time": "#{package.burst_time}/#{package.burst_time}"
-
-          }
-
-          add_queue(router_ip, router_username, router_password, payload)
-          Rails.logger.info "[ContentionRatioJob] Queue added for #{queue_name}"
-        end
-      rescue => e
-        Rails.logger.info "[ContentionRatioJob] Error for router #{router.name}: #{e.message}"
-      end
-    end
-
-
-
-    def fetch_active_users(ip, username, password)
-    uri = URI("http://#{ip}/rest/ppp/active")
-    req = Net::HTTP::Get.new(uri)
-    req.basic_auth(username, password)
-
-    res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
-    return [] unless res.is_a?(Net::HTTPSuccess)
-
-    JSON.parse(res.body)
-  rescue => e
-    Rails.logger.info "Failed to fetch active users: #{e.message}"
-    []
-  end
-
-  def queue_exists?(ip, username, password, queue_name)
-    uri = URI("http://#{ip}/rest/queue/simple/find?name=#{queue_name}")
-    req = Net::HTTP::Get.new(uri)
-    req.basic_auth(username, password)
-
-    res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
-
-    res.is_a?(Net::HTTPSuccess) && JSON.parse(res.body).any?
-  rescue => e
-    Rails.logger.info "Failed to check queue #{queue_name} existence: #{e.message}"
-    false
-  end
-
-  def add_queue(ip, username, password, payload)
-    uri = URI("http://#{ip}/rest/queue/simple/add")
-    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-    req.basic_auth(username, password)
-    req.body = payload.to_json
-
-    res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
-
-    unless res.is_a?(Net::HTTPSuccess)
-      raise "Failed to add queue: #{res.body}"
-    end
-  end
 
 
 
@@ -181,12 +85,15 @@ NasRouter.all.each do |router|
 
 
 
+
+
+    
 
 log_output = `journalctl -u loophole -n 200 --no-page`
 
 
     unless log_output.match(%r{https://([a-z0-9\-]+\.loophole\.site)})
-      # Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
+      Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
 
       # Restart cloudflared and your backend service
       `systemctl restart loophole`
@@ -212,7 +119,7 @@ log_output = `journalctl -u loophole -n 200 --no-page`
     #   Rails.logger.info "System is blocked. No action needed."
     # end
 
-# Rails.logger.info "Rehydrating wireguard"
+Rails.logger.info "Rehydrating wireguard"
     WireguardPeer.find_each do |peer|
       # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}, 192.168.50.47/32`
         if peer.private_ip.nil?
@@ -234,7 +141,7 @@ log_output = `journalctl -u loophole -n 200 --no-page`
 
 
         unless RUBY_PLATFORM =~ /linux/
-          # Rails.logger.info "SystemMetricsJob skipped: Not running on a linux system."
+          Rails.logger.info "SystemMetricsJob skipped: Not running on a linux system."
           
           sys = SystemMetric.first_or_initialize(
             cpu_usage: '0%',
@@ -310,7 +217,7 @@ log_output = `journalctl -u loophole -n 200 --no-page`
        
 
         # Log metrics for debugging
-        # Rails.logger.info "System Metrics: #{metrics}"
+        Rails.logger.info "System Metrics: #{metrics}"
 
         # Save to database
         sys = SystemMetric.first_or_initialize(
@@ -338,7 +245,7 @@ log_output = `journalctl -u loophole -n 200 --no-page`
           account_id: ActsAsTenant.current_tenant.id
         ) 
 
-        # Rails.logger.info "System Metrics saved: #{sys.inspect}"
+        Rails.logger.info "System Metrics saved: #{sys.inspect}"
       end
     end
   end
