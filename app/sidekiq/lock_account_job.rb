2bc406a206fdda63769c44fa227cec46ff8a4517
malcomowilla <owillamalcom@gmail.com>
2025-07-19 09:49:18 +0300

    free radius

--- app/sidekiq/lock_account_job.rb
+++ app/sidekiq/lock_account_job.rb
@@ -2,21 +2,26 @@ class LockAccountJob
   include Sidekiq::Job
   queue_as :default
 
-
-
   def perform
-    RadAcct.where.not(callingstationid: [nil, '']).where(framedprotocol: 'PPP').find_each do |radacct|
+    RadAcct.where(framedprotocol: 'PPP').where.not(callingstationid: [nil, '']).find_each do |radacct|
       subscription = Subscription.find_by(pppoe_username: radacct.username)
-
       next unless subscription
-      next if subscription.mac_address.present? # Already locked
-      next if radacct.callingstationid.blank?
 
-      # Save the MAC address to the subscription
-      subscription.update(mac_address: radacct.callingstationid)
-Rails.logger.info "Locked MAC address for subscription #{subscription.mac_adress}"
-      # Create the Radcheck rule to lock MAC
-      Radcheck.find_or_create_by!(
+      # Only proceed if RadCheck is not already created
+      existing_check = RadCheck.find_by(
+        username: subscription.ppoe_username,
+        radiusattribute: 'Calling-Station-Id'
+      )
+      next if existing_check.present?
+
+      # Save the MAC if not already stored
+      if subscription.mac_address.blank?
+        subscription.update(mac_address: radacct.callingstationid)
+        Rails.logger.info "MAC locked on subscription #{subscription.id}: #{subscription.mac_adress}"
+      end
+
+      # Create RadCheck to enforce sticky MAC
+      RadCheck.create!(
         username: subscription.ppoe_username,
         radiusattribute: 'Calling-Station-Id',
         op: '==',
@@ -25,7 +30,3 @@ Rails.logger.info "Locked MAC address for subscription #{subscription.mac_adress
     end
   end
 end
-
-
-
-
