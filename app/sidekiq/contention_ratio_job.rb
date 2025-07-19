c8328de1ea848065140454589981d5924e61e9f1
malcomowilla <owillamalcom@gmail.com>
2025-07-19 20:32:14 +0300

    mikrotik que

--- app/sidekiq/contention_ratio_job.rb
+++ app/sidekiq/contention_ratio_job.rb
@@ -192,6 +192,29 @@ class ContentionRatioJob
           Rails.logger.info "[ContentionRatioJob] Queue added for #{queue_name}"
         end
 
+
+        #   existing_queues = fetch_all_queues(router_ip, router_username, router_password)
+
+        # existing_queues.each do |queue|
+        #    queue_name = queue['name']
+        #   # next unless queue_name&.start_with?('queue_')
+
+        #   # username_part = queue_name.split('_')[1] # Extract PPPoE username
+        #   # next if active_usernames.include?(username_part)
+
+        #   # remove_queue(router_ip, router_username, router_password, queue_name)
+        #   # Rails.logger.info "[ContentionRatioJob] Removed stale queue #{queue_name}"
+        #   if queue_name.start_with?('queue_')
+        #     username_part = queue_name.split('_')[1] # Extract PPPoE username
+        #     if !active_usernames.include?(username_part)
+        #       Rails.logger.info "[ContentionRatioJob] Removing stale queue #{queue_name}"
+        #       remove_queue(router_ip, router_username, router_password, queue_name)
+        #     end
+              
+        #   end
+        # end
+
+existing_queues = fetch_all_queues(router_ip, router_username, router_password)
 Rails.logger.info "existing_queues: #{existing_queues}"
          existing_queues.each do |queue|
   queue_name = queue['name']
@@ -206,7 +229,6 @@ Rails.logger.info "existing_queues: #{existing_queues}"
     end
   end
 end
-
       rescue => e
         Rails.logger.info "[ContentionRatioJob] Error for router #{router.name}: #{e.message}"
       end
