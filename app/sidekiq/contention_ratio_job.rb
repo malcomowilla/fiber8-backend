852500bcaa14444d47f8b150f859955925d2a34b
malcomowilla <owillamalcom@gmail.com>
2025-07-19 16:04:29 +0300

    mikrotik que

--- app/sidekiq/contention_ratio_job.rb
+++ app/sidekiq/contention_ratio_job.rb
@@ -187,14 +187,10 @@ class ContentionRatioJob
           next unless queue_name&.start_with?('queue_')
 
           username_part = queue_name.split('_')[1] # Extract PPPoE username
-          # next if active_usernames.include?(username_part)
+          next if !active_usernames.include?(username_part)
 
-          # remove_queue(router_ip, router_username, router_password, queue_name)
-          # Rails.logger.info "[ContentionRatioJob] Removed stale queue #{queue_name}"
-          unless active_usernames.include?(username_part)
-  remove_queue(router_ip, router_username, router_password, queue_name)
-  Rails.logger.info "[ContentionRatioJob] Removed stale queue #{queue_name}"
-end
+          remove_queue(router_ip, router_username, router_password, queue_name)
+          Rails.logger.info "[ContentionRatioJob] Removed stale queue #{queue_name}"
         end
 
       rescue => e
