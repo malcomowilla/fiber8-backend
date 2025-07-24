24c0f69738c01c918f844d8e625e37362254def2
malcomowilla <owillamalcom@gmail.com>
2025-07-25 00:37:26 +0300

    realtime data

--- app/sidekiq/online_stats_broadcast_job.rb
+++ app/sidekiq/online_stats_broadcast_job.rb
@@ -5,15 +5,35 @@ class OnlineStatsBroadcastJob
   include Sidekiq::Job
   queue_as :default
 
-  
   def perform
-    # stats = generate_stats # however you get the stats
+    Rails.logger.info "Broadcasting online stats "
 
-    ActionCable.server.broadcast("online_stats_channel", { content: 'hello test' })
+    Account.find_each do |tenant| # Iterate over all tenants
+      ActsAsTenant.with_tenant(tenant) do
+Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
+    active_sessions = RadAcct.where(
+      acctstoptime: nil,
+      framedprotocol: 'PPP',
+      account_id: tenant.id
+    ).where('acctupdatetime > ?', 3.minutes.ago)
 
-  rescue => e
+    total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
+    total_upload   = active_sessions.sum("COALESCE(acctoutputoctets, 0)")
+
+    ActionCable.server.broadcast("online_stats_channel", {
+      active_user_count: active_sessions.count,
+      download_total: total_download,
+      upload_total: total_upload,
+      timestamp: Time.current.strftime("%I:%M:%S %p")
+    })
+  end
+end
+
+end
+
+rescue => e
     Rails.logger.error "ActionCable Broadcast Failed: #{e.class} - #{e.message}"
     Rails.logger.error e.backtrace.join("\n")
     raise e # still let Sidekiq retry if needed
-  end
+  
 end
