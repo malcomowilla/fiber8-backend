e7ddb913f935faab0a5a2531cab119fb038e888a
malcomowilla <owillamalcom@gmail.com>
2025-07-25 00:59:21 +0300

    realtime data

--- app/sidekiq/online_stats_broadcast_job.rb
+++ app/sidekiq/online_stats_broadcast_job.rb
@@ -8,8 +8,8 @@ class OnlineStatsBroadcastJob
   def perform
     Rails.logger.info "Broadcasting online stats "
 
-    Account.find_each do |tenant| # Iterate over all tenants
-      ActsAsTenant.with_tenant(tenant) do
+    # Account.find_each do |tenant| # Iterate over all tenants
+    #   ActsAsTenant.with_tenant(tenant) do
 Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
     active_sessions = RadAcct.where(
       acctstoptime: nil,
@@ -27,13 +27,13 @@ Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
       timestamp: Time.current.strftime("%I:%M:%S %p")
     })
   end
-end
+# end
 
-end
+# end
 
-rescue => e
-    Rails.logger.error "ActionCable Broadcast Failed: #{e.class} - #{e.message}"
-    Rails.logger.error e.backtrace.join("\n")
-    raise e # still let Sidekiq retry if needed
+# rescue => e
+#     Rails.logger.error "ActionCable Broadcast Failed: #{e.class} - #{e.message}"
+#     Rails.logger.error e.backtrace.join("\n")
+#     raise e # still let Sidekiq retry if needed
   
 end
