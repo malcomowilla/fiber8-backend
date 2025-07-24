b02bb3524693a85f27179d92bc90e0872fc2d6ff
malcomowilla <owillamalcom@gmail.com>
2025-07-24 23:37:48 +0300

    realtime data

--- app/sidekiq/online_stats_broadcast_job.rb
+++ app/sidekiq/online_stats_broadcast_job.rb
@@ -8,13 +8,11 @@ class OnlineStatsBroadcastJob
   def perform
     Rails.logger.info "Broadcasting online stats "
 
-    Account.find_each do |tenant| # Iterate over all tenants
-      ActsAsTenant.with_tenant(tenant) do
+    
 Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
     active_sessions = RadAcct.where(
       acctstoptime: nil,
       framedprotocol: 'PPP',
-      account_id: tenant.id
     ).where('acctupdatetime > ?', 3.minutes.ago)
 
     total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
@@ -27,6 +25,4 @@ Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
       timestamp: Time.current.strftime("%I:%M:%S %p")
     })
   end
-end
-end
 end
