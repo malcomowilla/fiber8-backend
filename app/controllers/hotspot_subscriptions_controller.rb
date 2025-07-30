6885136ec3740e96b0bce984e51dbc8e35eb7ae1
malcomowilla <owillamalcom@gmail.com>
2025-07-30 17:59:37 +0300

    hotspot autologin

--- app/controllers/hotspot_subscriptions_controller.rb
+++ app/controllers/hotspot_subscriptions_controller.rb
@@ -30,8 +30,8 @@ def set_tenant
 
 Rails.logger.info "IP from check session: #{ip}"
   # Step 1: Check if there's an active session in radacct (acctstoptime is nil = session still active)
-  session = RadAcct.find_by(framedipaddress: ip, framedprotocol: '') || RadAcct.find_by(callingstationid: mac, framedprotocol: '')
-
+session = RadAcct.where(framedipaddress: ip, acctstoptime: nil).order(acctupdatetime: :desc).first ||
+          RadAcct.where(callingstationid: mac, acctstoptime: nil).order(acctupdatetime: :desc).first
   if session
     voucher = HotspotVoucher.find_by(voucher: session.username)
 
