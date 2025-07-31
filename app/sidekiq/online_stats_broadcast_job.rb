ee7a96a71dbc25ae57f594196a586825ea34ded5
malcomowilla <owillamalcom@gmail.com>
2025-07-31 16:06:31 +0300

    tickets

--- app/sidekiq/online_stats_broadcast_job.rb
+++ app/sidekiq/online_stats_broadcast_job.rb
@@ -1,39 +1,39 @@
 
 
 
-class OnlineStatsBroadcastJob 
-  include Sidekiq::Job
-  queue_as :default
+# class OnlineStatsBroadcastJob 
+#   include Sidekiq::Job
+#   queue_as :default
 
-  def perform
-    Rails.logger.info "Broadcasting online stats "
+#   def perform
+#     Rails.logger.info "Broadcasting online stats "
 
-    Account.find_each do |tenant| # Iterate over all tenants
-      ActsAsTenant.with_tenant(tenant) do
-Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
-Rails.logger.info "Account ID: #{tenant.id}"
+#     Account.find_each do |tenant| # Iterate over all tenants
+#       ActsAsTenant.with_tenant(tenant) do
+# Rails.logger.info "Broadcasting online stats for #{tenant.subdomain}"
+# Rails.logger.info "Account ID: #{tenant.id}"
 
-    active_sessions = RadAcct.where(
-      acctstoptime: nil,
-      framedprotocol: 'PPP',
-  #  account_id: tenant.id # safer than ActsAsTenant.current_tenant.id
+#     active_sessions = RadAcct.where(
+#       acctstoptime: nil,
+#       framedprotocol: 'PPP',
+#   #  account_id: tenant.id # safer than ActsAsTenant.current_tenant.id
 
-    ).where('acctupdatetime > ?', 3.minutes.ago)
+#     ).where('acctupdatetime > ?', 3.minutes.ago)
 
-    total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
-    total_upload   = active_sessions.sum("COALESCE(acctoutputoctets, 0)")
-Rails.logger.info "Account ID: #{tenant.id}"
+#     total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
+#     total_upload   = active_sessions.sum("COALESCE(acctoutputoctets, 0)")
+# Rails.logger.info "Account ID: #{tenant.id}"
 
-    ActionCable.server.broadcast("online_stats_channel", {
-      active_user_count: active_sessions.count,
-      download_total: total_download,
-      upload_total: total_upload,
-      # timestamp: Time.current.strftime("%I:%M:%S %p")
-    })
-  end
-end
+#     ActionCable.server.broadcast("online_stats_channel", {
+#       active_user_count: active_sessions.count,
+#       download_total: total_download,
+#       upload_total: total_upload,
+#       # timestamp: Time.current.strftime("%I:%M:%S %p")
+#     })
+#   end
+# end
 
-end
+# end
 
 
-end
+# end
