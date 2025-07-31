10d5a04d16a566cc94814764dbb1390810556941
malcomowilla <owillamalcom@gmail.com>
2025-07-31 16:43:28 +0300

    tickets

--- app/sidekiq/tickets_job.rb
+++ app/sidekiq/tickets_job.rb
@@ -2,25 +2,57 @@
 
 
 
+# class TicketsJob
+#   include Sidekiq::Job
+#   queue_as :default
+  
+#   def perform
+#     Account.find_each do |tenant|
+#       ActsAsTenant.with_tenant(tenant) do
+# Rails.logger.info "TicketsJob started"
+# tickets = SupportTicket.all
+#     tickets.each do |ticket|  
+# Rails.logger.info "Ticket #{ticket.id} status: #{ticket.status} ticket_number: #{ticket.ticket_number}"
+#     end
+
+
+#   end
+# end
+#   end
+
+
+
+#   end
+
+
 class TicketsJob
   include Sidekiq::Job
   queue_as :default
-  
+
   def perform
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
-Rails.logger.info "TicketsJob started"
-tickets = SupportTicket.all
-    tickets.each do |ticket|  
-Rails.logger.info "Ticket #{ticket.id} status: #{ticket.status} ticket_number: #{ticket.ticket_number}"
+        Rails.logger.info "TicketsJob started for #{tenant.subdomain}"
+
+        tickets = SupportTicket.all
+
+        tickets_data = tickets.map do |ticket|
+          {
+            id: ticket.id,
+            status: ticket.status,
+            ticket_number: ticket.ticket_number
+          }
+        end
+
+        # Broadcast to the TicketsChannel for this tenant
+        TicketsChannel.broadcast_to(
+          tenant,
+          {
+            tickets: tickets_data,
+            timestamp: Time.current
+          }
+        )
+      end
     end
-
-
   end
 end
-  end
-
-  
-
-  end
-
