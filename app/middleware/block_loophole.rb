5d870bfc29013a5798fddc96419069950539fc5c
malcomowilla <owillamalcom@gmail.com>
2025-08-01 18:06:24 +0300

    block

--- app/middleware/block_loophole.rb
+++ app/middleware/block_loophole.rb
@@ -7,13 +7,16 @@ class BlockLoophole
     request = ActionDispatch::Request.new(env)
 
     forbidden_hosts = [
-      /.*\.loophole\.site$/,
-      /eadb17b24faecde730f4d59914c23431\.loophole\.site/,
-      /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ # match direct IP
+      /\A[a-z0-9-]+\.loophole\.site\z/i,
+      /\Aeadb17b24faecde730f4d59914c23431\.loophole\.site\z/,
+      /\A(?!127\.0\.0\.1)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\z/ # exclude 127.0.0.1
     ]
 
-    if forbidden_hosts.any? { |regex| request.host =~ regex || request.referer.to_s =~ regex }
-      Rails.logger.warn("Blocked loophole request: #{request.host}, referer: #{request.referer}")
+    host = request.host.to_s
+    referer_host = URI.parse(request.referer).host rescue nil
+
+    if forbidden_hosts.any? { |regex| host =~ regex || referer_host =~ regex }
+      Rails.logger.warn("Blocked loophole request: #{host}, referer: #{request.referer}")
       return [403, { 'Content-Type' => 'text/plain' }, ['Forbidden']]
     end
 
