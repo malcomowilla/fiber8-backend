0c43c94b75c28f5064b4cb45c5c86fbab0b708ae
malcomowilla <owillamalcom@gmail.com>
2025-08-20 20:54:45 +0300

    ireguard

--- app/sidekiq/rehydrate_wireguard_job.rb
+++ app/sidekiq/rehydrate_wireguard_job.rb
@@ -8,14 +8,10 @@ class RehydrateWireguardJob
     WireguardPeer.find_each do |peer|
       # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
             if peer.private_ip.nil?
-              `wg set wg0 listen-port 51820`
-
-              `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips} listen-port 51820`
+              `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
               
             else
-                   `wg set wg0 listen-port 51820`
-
-               `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},#{peer.private_ip} listen-port 51820`
+               `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},#{peer.private_ip}`
 
             end
 
