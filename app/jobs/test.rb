c952891c30c3c43b1dd76e3736f751fafea71b34
malcomowilla <owillamalcom@gmail.com>
2025-08-22 16:39:04 +0300

    subnet range

---
+++ app/jobs/test.rb
@@ -0,0 +1,6 @@
+
+ 
+private_ip = "10.8.0.0/16\n"
+puts private_ip
+puts private_ip.split("\n")
+puts private_ip.split("\n")[0]
--- app/serializers/ip_network_serializer.rb
+++ app/serializers/ip_network_serializer.rb
@@ -28,13 +28,23 @@ class IpNetworkSerializer < ActiveModel::Serializer
     nil
   end
 
+  # def client_host_range
+  #   network = IPAddr.new("#{object.network}/#{object.subnet_mask}").to_range
+  #   fist_ip = network.first.to_s
+  #   last_ip = network.last.to_s
+  #   "#{fist_ip} - #{last_ip}"
+  # end
+
+
   def client_host_range
-    network = IPAddr.new("#{object.network}/#{object.subnet_mask}").to_range
-    fist_ip = network.first.to_s
-    last_ip = network.last.to_s
-    "#{fist_ip} - #{last_ip}"
+ result = `ipcalc #{object.network}/#{object.subnet_mask}`
+  host_min = result[/HostMin:\s+(\S+)/, 1]
+  host_max = result[/HostMax:\s+(\S+)/, 1]
+  "#{host_min} - #{host_max}" if host_min && host_max    
+    
   end
 
+  
   def total_ip_addresses
     range = IPAddr.new("#{object.network}/#{object.subnet_mask}").to_range
     [range.count - 2, 0].max  # Prevents negative value for /31 or /32
