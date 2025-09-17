b3d123dbe1e1c567973aa1631d73187dccb9943b
malcomowilla <owillamalcom@gmail.com>
2025-09-17 12:06:29 +0300

    invoice

--- app/sidekiq/generate_invoice_job.rb
+++ app/sidekiq/generate_invoice_job.rb
@@ -8,7 +8,7 @@ class GenerateInvoiceJob
         # Process hotspot plan
         # 
         if !tenant.hotspot_plan.name == 'Free Trial'
-        if tenant.hotspot_plan.present? && tenant.hotspot_plan.expiry > Time.current
+        if tenant.hotspot_plan.present? && tenant.hotspot_plan.expiry < Time.current
           process_hotspot_plan_invoice(tenant, tenant.hotspot_plan.name, tenant.hotspot_plan.price, 
           tenant.hotspot_plan.expiry_days)
         end
@@ -17,7 +17,7 @@ class GenerateInvoiceJob
 
         # Process PPPoE plan
          if !tenant.pp_poe_plan.name == 'Free Trial'
-        if tenant.pp_poe_plan.present? && tenant.pp_poe_plan.expiry > Time.current
+        if tenant.pp_poe_plan.present? && tenant.pp_poe_plan.expiry < Time.current
           process_pppoe_plan_invoice(tenant, tenant.pp_poe_plan.name, tenant.pp_poe_plan.price, 
           tenant.pp_poe_plan.expiry_days)
         end
