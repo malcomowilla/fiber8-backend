6e60ecb43891941f2a4e7302df8335ea101a3984
malcomowilla <owillamalcom@gmail.com>
2025-10-07 15:01:00 +0300

    company id

--- app/sidekiq/company_id_job.rb
+++ app/sidekiq/company_id_job.rb
@@ -1,32 +1,29 @@
-
-
-class CompanyIdJob 
+class CompanyIdJob
   include Sidekiq::Job
   queue_as :default
 
   def perform
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
-        
- Rails.logger.info "Processing company id...... => #{tenant.subdomain}"
-         CompanyId.where(company_id: nil).find_each do |company|
-          company.first_or_initialize(company_id: generate_company_id,
-          
-          account_id: tenant.id
-          )
+        Rails.logger.info "Processing company ID for tenant => #{tenant.subdomain}"
+
+        # find or create a company_id record for this account
+        company_id_record = CompanyId.find_or_initialize_by(account_id: tenant.id)
+
+        if company_id_record.company_id.blank?
+          company_id_record.company_id = generate_company_id
+          company_id_record.save!
+          Rails.logger.info "Generated company ID #{company_id_record.company_id} for #{tenant.subdomain}"
+        else
+          Rails.logger.info "Company ID already exists for #{tenant.subdomain}"
         end
-
-         Rails.logger.info "Processing company id...... after => #{tenant.subdomain}"
-
-
       end
-      end
+    end
   end
 
-
+  private
 
   def generate_company_id
     "CID#{rand(100..999)}"
   end
-
 end
