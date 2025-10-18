b28e3f4130ce196fe27d63af298c4665aef3d07c
malcomowilla <owillamalcom@gmail.com>
2025-10-18 23:39:43 +0300

    invoicing

--- app/sidekiq/load_radius_job.rb
+++ app/sidekiq/load_radius_job.rb
@@ -1,48 +1,48 @@
 
-# class LoadRadiusJob 
+class LoadRadiusJob 
 
-#   include Sidekiq::Job
-#   queue_as :default
-
-
+  include Sidekiq::Job
+  queue_as :default
 
 
-#   def perfom
-#     Account.find_each do |tenant|
-#       ActsAsTenant.with_tenant(tenant) do
 
 
-#           Na.unscoped.where(
+  def perfom
+    Account.find_each do |tenant|
+      ActsAsTenant.with_tenant(tenant) do
+
+
+          Na.unscoped.where(
        
-#           shortname: nil, 
-#           secret: nil,
+          shortname: nil, 
+          secret: nil,
     
-#         account_id: nil
-#         ).find_each do |radius|
-#           begin
-#             radius.update!(account_id: tenant.id,
-#             secret: '$rends6$#b8',
-#              shortname: 'superadmin',
-#             )
-#           rescue => e
-#             # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
-#           end
-#         end
+        account_id: nil
+        ).find_each do |radius|
+          begin
+            radius.update!(account_id: tenant.id,
+            secret: '$rends6$#b8',
+             shortname: 'superadmin',
+            )
+          rescue => e
+            # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
+          end
+        end
         
 
 
 
 
-#       end
-#     end
+      end
+    end
 
 
-#   end
+  end
 
 
 
 
-# end
+end
 
 
 
--- app/sidekiq/rehydrate_wireguard_job.rb
+++ app/sidekiq/rehydrate_wireguard_job.rb
@@ -1,24 +1,24 @@
-# class RehydrateWireguardJob
-#   include Sidekiq::Job
-#     queue_as :default
+class RehydrateWireguardJob
+  include Sidekiq::Job
+    queue_as :default
 
 
-#    def perform
-#     Rails.logger.info "Rehydrating wireguard"
-#     WireguardPeer.find_each do |peer|
-#       # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
-#             if peer.private_ip.nil?
-#               `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
+   def perform
+    Rails.logger.info "Rehydrating wireguard"
+    WireguardPeer.find_each do |peer|
+      # `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
+            if peer.private_ip.nil?
+              `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips}`
               
-#             else
-#                `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},#{peer.private_ip}`
+            else
+               `wg set wg0 peer #{peer.public_key} allowed-ips #{peer.allowed_ips},#{peer.private_ip}`
 
-#             end
+            end
 
 
-#     end
-#   end
-# end
+    end
+  end
+end
 
 
 
