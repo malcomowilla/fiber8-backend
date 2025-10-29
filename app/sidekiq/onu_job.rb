9d7c84cab2e64c1161532ff1403760fc4c74251f
malcomowilla <owillamalcom@gmail.com>
2025-10-29 21:43:58 +0300

    invoice

--- app/sidekiq/onu_job.rb
+++ app/sidekiq/onu_job.rb
@@ -2,20 +2,20 @@
 
 
 
-class ONUJob 
-  include Sidekiq::Job
-  queue_as :onu
+# class ONUJob 
+#   include Sidekiq::Job
+#   queue_as :onu
   
-  def perform(onu_id)
-    onu = ONU.find(onu_id)
-    onu.update_status
+#   def perform(onu_id)
+#     onu = ONU.find(onu_id)
+#     onu.update_status
     
-    if onu.status == 'active'
-      onu.update_status
+#     if onu.status == 'active'
+#       onu.update_status
       
-      if onu.status == 'active'
-        ONUJob.perform_later(onu.id)
-      end
-    end
-   end
-end
+#       if onu.status == 'active'
+#         ONUJob.perform_later(onu.id)
+#       end
+#     end
+#    end
+# end
--- app/sidekiq/rad_sessions_job.rb
+++ app/sidekiq/rad_sessions_job.rb
@@ -1,146 +1,146 @@
 
 
 
-class RadSessionsJob
-  include Sidekiq::Job
-  queue_as :radacct
-  # sidekiq_options retry: false
+# class RadSessionsJob
+#   include Sidekiq::Job
+#   queue_as :radacct
+#   # sidekiq_options retry: false
   
-   def perform
-    Account.find_each do |account| # Iterate over all tenants
-      ActsAsTenant.with_tenant(account) do
-  threshold_time = 3.minutes.ago
+#    def perform
+#     Account.find_each do |account| # Iterate over all tenants
+#       ActsAsTenant.with_tenant(account) do
+#   threshold_time = 3.minutes.ago
 
-  online = 0
-  offline = 0
+#   online = 0
+#   offline = 0
 
-  account.subscribers.find_each do |subscriber|
-    # Assume offline unless proven online
-    subscriber_online = false
+#   account.subscribers.find_each do |subscriber|
+#     # Assume offline unless proven online
+#     subscriber_online = false
 
-    subscriber.subscriptions.each do |subscription|
-      next unless subscription.ip_address.present?
+#     subscriber.subscriptions.each do |subscription|
+#       next unless subscription.ip_address.present?
 
-      if RadAcct.where(
-        acctstoptime: nil,
-        framedprotocol: 'PPP',
-        framedipaddress: subscription.ip_address
-      ).where('acctupdatetime > ?', threshold_time).exists?
-        subscriber_online = true
-        break
-      end
-    end
-
-
-
-
-
-    if subscriber_online
-      online += 1
-    else
-      offline += 1
-    end
-  end
+#       if RadAcct.where(
+#         acctstoptime: nil,
+#         framedprotocol: 'PPP',
+#         framedipaddress: subscription.ip_address
+#       ).where('acctupdatetime > ?', threshold_time).exists?
+#         subscriber_online = true
+#         break
+#       end
+#     end
 
 
 
 
-    active_sessions = RadAcct.where(
-    framedprotocol: 'PPP'
-  )
+
+#     if subscriber_online
+#       online += 1
+#     else
+#       offline += 1
+#     end
+#   end
 
 
-  total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
-  total_upload   = active_sessions.sum("COALESCE(acctoutputoctets, 0)")
-  total_bytes    = total_download + total_upload
 
-  radacct_data = {
-    online_radacct: online,
-    offline_radacct: offline,
+
+#     active_sessions = RadAcct.where(
+#     framedprotocol: 'PPP'
+#   )
+
+
+#   total_download = active_sessions.sum("COALESCE(acctinputoctets, 0)")
+#   total_upload   = active_sessions.sum("COALESCE(acctoutputoctets, 0)")
+#   total_bytes    = total_download + total_upload
+
+#   radacct_data = {
+#     online_radacct: online,
+#     offline_radacct: offline,
   
-    timestamp: Time.current
-  }
+#     timestamp: Time.current
+#   }
 
 
 
-  bandwidth_data = {
-     total_bandwidth: format_bytes(total_bytes),
-    total_download: format_bytes(total_download),
-    total_upload: format_bytes(total_upload),
-  }
+#   bandwidth_data = {
+#      total_bandwidth: format_bytes(total_bytes),
+#     total_download: format_bytes(total_download),
+#     total_upload: format_bytes(total_upload),
+#   }
 
 
 
-  active_sessions = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago) 
+#   active_sessions = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago) 
 
-  total_bytes = 0
+#   total_bytes = 0
 
-  active_user_data = active_sessions.map do |session|
-    download_bytes = session.acctinputoctets || 0
-    upload_bytes = session.acctoutputoctets || 0
-    session_total = download_bytes + upload_bytes
-    total_bytes += session_total
+#   active_user_data = active_sessions.map do |session|
+#     download_bytes = session.acctinputoctets || 0
+#     upload_bytes = session.acctoutputoctets || 0
+#     session_total = download_bytes + upload_bytes
+#     total_bytes += session_total
 
-    {
-      username: session.username,
-      ip_address: session.framedipaddress.to_s,
-      mac_address: session.callingstationid,
-      up_time: format_uptime(session.acctsessiontime),
-      download: format_bytes(download_bytes),
-      upload: format_bytes(upload_bytes),
-      start_time: session.acctstarttime.strftime("%B %d, %Y at %I:%M %p"),
-      nas_port: session.nasportid
-    }
-  end
+#     {
+#       username: session.username,
+#       ip_address: session.framedipaddress.to_s,
+#       mac_address: session.callingstationid,
+#       up_time: format_uptime(session.acctsessiontime),
+#       download: format_bytes(download_bytes),
+#       upload: format_bytes(upload_bytes),
+#       start_time: session.acctstarttime.strftime("%B %d, %Y at %I:%M %p"),
+#       nas_port: session.nasportid
+#     }
+#   end
 
-  hotspot_data = {
-active_user_count: active_user_data.size,
-  total_upload: format_bytes(total_upload),
-  total_download: format_bytes(total_download),
-  total_bandwidth: format_bytes(total_bytes),
-  users: active_user_data
-  }
+#   hotspot_data = {
+# active_user_count: active_user_data.size,
+#   total_upload: format_bytes(total_upload),
+#   total_download: format_bytes(total_download),
+#   total_bandwidth: format_bytes(total_bytes),
+#   users: active_user_data
+#   }
 
-  HotspotChannel.broadcast_to(account, 
-    hotspot_data
+#   HotspotChannel.broadcast_to(account, 
+#     hotspot_data
  
-  )
+#   )
 
-  RadacctChannel.broadcast_to(account, radacct_data)
-  BandwidthChannel.broadcast_to(account, bandwidth_data)
-end
-end
-end
+#   RadacctChannel.broadcast_to(account, radacct_data)
+#   BandwidthChannel.broadcast_to(account, bandwidth_data)
+# end
+# end
+# end
 
 
 
-def format_bytes(bytes)
-      units = ['B', 'KB', 'MB', 'GB', 'TB']
-      return '0 B' if bytes.zero?
+# def format_bytes(bytes)
+#       units = ['B', 'KB', 'MB', 'GB', 'TB']
+#       return '0 B' if bytes.zero?
     
-      exp = (Math.log(bytes) / Math.log(1024)).to_i
-      size = bytes / (1024.0**exp)
-      "%.2f #{units[exp]}" % size
+#       exp = (Math.log(bytes) / Math.log(1024)).to_i
+#       size = bytes / (1024.0**exp)
+#       "%.2f #{units[exp]}" % size
     
     
-  end
+#   end
 
 
 
 
-  def format_uptime(seconds)
-  return '0s' if seconds.nil?
+#   def format_uptime(seconds)
+#   return '0s' if seconds.nil?
 
-  mm, ss = seconds.divmod(60)
-  hh, mm = mm.divmod(60)
-  dd, hh = hh.divmod(24)
+#   mm, ss = seconds.divmod(60)
+#   hh, mm = mm.divmod(60)
+#   dd, hh = hh.divmod(24)
 
-  parts = []
-  parts << "#{dd}d" if dd > 0
-  parts << "#{hh}h" if hh > 0
-  parts << "#{mm}m" if mm > 0
-  parts << "#{ss}s"
-  parts.join(' ')
-    end
+#   parts = []
+#   parts << "#{dd}d" if dd > 0
+#   parts << "#{hh}h" if hh > 0
+#   parts << "#{mm}m" if mm > 0
+#   parts << "#{ss}s"
+#   parts.join(' ')
+#     end
     
-end
+# end
