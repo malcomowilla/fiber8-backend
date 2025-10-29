d54730a2caf7ac6647da34122d8f3da477912590
malcomowilla <owillamalcom@gmail.com>
2025-10-29 21:33:05 +0300

    invoice

--- app/sidekiq/generate_invoice_job.rb
+++ app/sidekiq/generate_invoice_job.rb
@@ -8,10 +8,10 @@ class GenerateInvoiceJob
         # Rails.logger.info "Processing plan invoice for => #{tenant.subdomain}"
 
                   
+          Rails.logger.info "Processing hotspot and ppoe plan invoice for => #{tenant.subdomain}"
 
         if tenant.hotspot_plan&.name.present? && tenant.hotspot_plan.name != 'Free Trial'
         if tenant.hotspot_plan&.expiry.present? && tenant.hotspot_plan.expiry < Time.current
-          Rails.logger.info "Processing hotspot plan invoice for => #{tenant.subdomain}"
 
            if hotspot_plan&.last_invoiced_at.nil? || tenant.hotspot_plan.last_invoiced_at < tenant.hotspot_plan.expiry
           tenant.hotspot_plan.update!(last_invoiced_at: Time.current)
@@ -27,7 +27,6 @@ class GenerateInvoiceJob
         # Process PPPoE plan
          if tenant.pp_poe_plan&.name.present? && tenant.pp_poe_plan.name != 'Free Trial'
          if tenant.pp_poe_plan&.expiry.present? && tenant.pp_poe_plan.expiry < Time.current
-          Rails.logger.info "Processing pppoe plan invoice for => #{tenant.subdomain}"
 
                      if tenant.pp_poe_plan&.last_invoiced_at.nil? || tenant.pp_poe_plan.last_invoiced_at < tenant.pp_poe_plan.expiry
                       tenant.pp_poe_plan.update!(last_invoiced_at: Time.current)
