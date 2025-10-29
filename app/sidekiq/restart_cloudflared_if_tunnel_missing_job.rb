9a130267257bf98c22000488423d09a053c085fa
malcomowilla <owillamalcom@gmail.com>
2025-10-29 22:21:59 +0300

    invoice

--- app/sidekiq/restart_cloudflared_if_tunnel_missing_job.rb
+++ app/sidekiq/restart_cloudflared_if_tunnel_missing_job.rb
@@ -1,24 +1,24 @@
-# class RestartCloudflaredIfTunnelMissingJob
-#   include Sidekiq::Job
-#   queue_as :default
+class RestartCloudflaredIfTunnelMissingJob
+  include Sidekiq::Job
+  queue_as :default
 
-#    def perform
-#     log_output = `journalctl -u loophole -n 200 --no-page`
+   def perform
+    log_output = `journalctl -u loophole -n 200 --no-page`
 
 
-#     unless log_output.match(%r{https://([a-z0-9\-]+\.loophole\.site)})
-#       Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
+    unless log_output.match(%r{https://([a-z0-9\-]+\.loophole\.site)})
+      Rails.logger.warn "[Tunnel Monitor] No valid Cloudflare tunnel found. Restarting services..."
 
-#       # Restart cloudflared and your backend service
-#       `systemctl restart loophole`
-#       `systemctl restart aitechs-fiber8-backend`
+      # Restart cloudflared and your backend service
+      `systemctl restart loophole`
+      `systemctl restart aitechs-fiber8-backend`
 
-#       Rails.logger.info "[Tunnel Monitor] Services restarted successfully."
-#     else
-#       Rails.logger.info "[Tunnel Monitor] Tunnel is active. No action needed."
-#     end
+      Rails.logger.info "[Tunnel Monitor] Services restarted successfully."
+    else
+      Rails.logger.info "[Tunnel Monitor] Tunnel is active. No action needed."
+    end
 
 
     
-#   end
-# end
+  end
+end
