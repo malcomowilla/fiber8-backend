22ff2e992d51cf7c17145bf4adc2516f92e24f2e
malcomowilla <owillamalcom@gmail.com>
2025-10-30 17:09:25 +0300

    sidekiq jobs

--- app/sidekiq/router_ping_job.rb
+++ app/sidekiq/router_ping_job.rb
@@ -97,113 +97,113 @@
 
 
 
-# require 'open3'
+require 'open3'
 
-# class RouterPingJob
-#   include Sidekiq::Job
-#   queue_as :default
+class RouterPingJob
+  include Sidekiq::Job
+  queue_as :default
 
-#   def perform
+  def perform
 
 
 
 
 
     
-#     Account.find_each do |tenant| # Iterate over all tenants
-#       ActsAsTenant.with_tenant(tenant) do
+    Account.find_each do |tenant| # Iterate over all tenants
+      ActsAsTenant.with_tenant(tenant) do
 
         
-#               subscriptions = Subscription.where.not(ip_address: [nil, ''])
-#               subscriptions.each do |subscription|
-#         # Process RadAcct records with nil account_id
-#         nil_radacct_count = RadAcct.unscoped.where(
+              subscriptions = Subscription.where.not(ip_address: [nil, ''])
+              subscriptions.each do |subscription|
+        # Process RadAcct records with nil account_id
+        nil_radacct_count = RadAcct.unscoped.where(
         
-#           framedipaddress: subscription.ip_address,
-#           username: subscription.ppoe_username,
-#           account_id: nil
-#         ).count
-#         Rails.logger.info "Found #{nil_radacct_count} RadAcct records with nil account_id for tenant #{tenant.id}"
+          framedipaddress: subscription.ip_address,
+          username: subscription.ppoe_username,
+          account_id: nil
+        ).count
+        Rails.logger.info "Found #{nil_radacct_count} RadAcct records with nil account_id for tenant #{tenant.id}"
 
-#         RadAcct.unscoped.where(
-#         framedipaddress: subscription.ip_address,
-#         username: subscription.ppoe_username,
-#         account_id: nil
-#         ).find_each do |radacct|
-#           begin
-#             radacct.update!(account_id: tenant.id)
-#           rescue => e
-#             # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
-#           end
-#         end
-#         end
-
-
+        RadAcct.unscoped.where(
+        framedipaddress: subscription.ip_address,
+        username: subscription.ppoe_username,
+        account_id: nil
+        ).find_each do |radacct|
+          begin
+            radacct.update!(account_id: tenant.id)
+          rescue => e
+            # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
+          end
+        end
+        end
 
 
 
 
-#    hotspot_subscriptions = HotspotVoucher.where.not(voucher: [nil, ''])
-#                hotspot_subscriptions.each do |subscription|
-#         # Process RadAcct records with nil account_id
-#         nil_radacct_count = RadAcct.unscoped.where(
+
+
+   hotspot_subscriptions = HotspotVoucher.where.not(voucher: [nil, ''])
+               hotspot_subscriptions.each do |subscription|
+        # Process RadAcct records with nil account_id
+        nil_radacct_count = RadAcct.unscoped.where(
         
-#           username: subscription.voucher,
-#           account_id: nil
-#         ).count
-#         Rails.logger.info "Found #{nil_radacct_count} RadAcct records with nil account_id for tenant otspot voucher #{tenant.id}"
+          username: subscription.voucher,
+          account_id: nil
+        ).count
+        Rails.logger.info "Found #{nil_radacct_count} RadAcct records with nil account_id for tenant otspot voucher #{tenant.id}"
 
-#         RadAcct.unscoped.where(
-#            username: subscription.voucher,
-#         account_id: nil
-#         ).find_each do |radacct|
-#           begin
-#             radacct.update!(account_id: tenant.id)
-#           rescue => e
-#             # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
-#           end
-#         end
-#         end
-
-
+        RadAcct.unscoped.where(
+           username: subscription.voucher,
+        account_id: nil
+        ).find_each do |radacct|
+          begin
+            radacct.update!(account_id: tenant.id)
+          rescue => e
+            # Rails.logger.error "Failed to update radacct with id #{radacct.id}: #{e.message}"
+          end
+        end
+        end
 
 
 
 
 
 
-#         # Check router status
-#         nas_routers = NasRouter.all
-#         nas_routers.each do |nas_router|
-#           ip_address = nas_router.ip_address
-#           Rails.logger.info "Pinging router at #{ip_address} for tenant #{tenant.id}"
 
-#           begin
-#             output, status = Open3.capture2e("ping -c 3 #{ip_address}")
-#             reachable = status.success?
 
-#             if reachable
-#               # Rails.logger.info "Ping successful: #{output}"
-#             else
-#               # Rails.logger.warn "Ping failed: #{output}"
-#             end
+        # Check router status
+        nas_routers = NasRouter.all
+        nas_routers.each do |nas_router|
+          ip_address = nas_router.ip_address
+          Rails.logger.info "Pinging router at #{ip_address} for tenant #{tenant.id}"
+
+          begin
+            output, status = Open3.capture2e("ping -c 3 #{ip_address}")
+            reachable = status.success?
+
+            if reachable
+              # Rails.logger.info "Ping successful: #{output}"
+            else
+              # Rails.logger.warn "Ping failed: #{output}"
+            end
 
        
-#         RouterStatus.find_or_initialize_by(
-#               tenant_id: tenant.id,
-#               ip: ip_address
-#             ).update(
-#               reachable: reachable,
-#               response: output,
-#               checked_at: Time.current
-#             )
-#             # Rails.logger.info "Router status updated for tenant #{tenant.id}"
-#           rescue StandardError => e
-#             # Rails.logger.error "RouterPingJob failed for tenant #{tenant.id}: #{e.message}"
-#             # Rails.logger.error e.backtrace.join("\n")
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
+        RouterStatus.find_or_initialize_by(
+              tenant_id: tenant.id,
+              ip: ip_address
+            ).update(
+              reachable: reachable,
+              response: output,
+              checked_at: Time.current
+            )
+            # Rails.logger.info "Router status updated for tenant #{tenant.id}"
+          rescue StandardError => e
+            # Rails.logger.error "RouterPingJob failed for tenant #{tenant.id}: #{e.message}"
+            # Rails.logger.error e.backtrace.join("\n")
+          end
+        end
+      end
+    end
+  end
+end
