2e05ba537e379fe9cf1d39ea587a27b46b750ba3
malcomowilla <owillamalcom@gmail.com>
2025-10-30 17:39:40 +0300

    send sms invoice

--- app/sidekiq/generate_invoice_job.rb
+++ app/sidekiq/generate_invoice_job.rb
@@ -27,6 +27,7 @@ if existing_invoice.nil? ||  existing_invoice.last_invoiced_at < 30.days.ago
     tenant.hotspot_plan.expiry
   )
 
+  # send_expiration_sms(tenant.hotspot_plan.phone, tenant.hotspot_plan.expiry, existing_invoice.invoice_number, tenant)
 else
     Rails.logger.info "Skipping invoice for #{tenant.subdomain} — already invoiced hotspot."
 end
@@ -84,7 +85,7 @@ end
 def process_hotspot_plan_invoice(tenant, plan_name, plan_amount, expiry_days, expiry)
     
 Rails.logger.info "Processing hotspot plan invoice for => #{tenant.subdomain}"
-      Invoice.create!(
+invoice =  Invoice.create!(
         invoice_number: generate_invoice_number,
         plan_name: "Hotspot Plan #{plan_name}",
          invoice_date: Time.current,
@@ -96,6 +97,9 @@ Rails.logger.info "Processing hotspot plan invoice for => #{tenant.subdomain}"
         last_invoiced_at: Time.current,
 
       )
+
+      # send_expiration_sms(phone_number, due_date, invoice_number, username, plan_name)
+       send_expiration_sms(tenant.users.where(role: "super_administrator").first.phone_number, invoice.due_date, invoice.invoice_number, tenant.users.where(role: "super_administrator").first.username, invoice.plan_name)
     
 end
 
@@ -107,7 +111,7 @@ end
   def process_pppoe_plan_invoice(tenant, plan_name, plan_amount, expiry_days, expiry)
     Rails.logger.info "Processing PPPoE plan invoice for => #{tenant.subdomain}"
 
-      Invoice.create!(
+  invoice = Invoice.create!(
         invoice_number: generate_invoice_number,
         plan_name: "PPPoE Plan #{plan_name}",
         invoice_date: Time.current,
@@ -119,6 +123,9 @@ end
         last_invoiced_at: Time.current,
 
       )
+
+       send_expiration_sms(tenant.users.where(role: "super_administrator").first.phone_number, invoice.due_date, invoice.invoice_number, tenant.users.where(role: "super_administrator").first.username, invoice.plan_name)
+
     
   end
 
@@ -131,6 +138,110 @@ end
 
 
 
+
+
+
+
+  def send_expiration_sms(phone_number, due_date, invoice_number, username, plan_name)
+    provider = ActsAsTenant.current_tenant.sms_provider_setting.sms_provider
+    
+
+    case provider
+    when 'TextSms'
+      send_expiration_text_sms(phone_number, due_date, invoice_number, username, plan_name)
+    when 'SMS leopard'
+      send_expiration(phone_number, due_date, invoice_number, username, plan_name)
+    else
+      Rails.logger.info "No valid SMS provider configured"
+    end
+  end
+
+  
+
+  def send_expiration(phone_number, due_date, invoice_number, username, plan_name)
+
+    # provider = ActsAsTenant.current_tenant.sms_provider_setting.sms_provider
+
+    api_key = SmsSetting.find_by(sms_provider: 'SMS leopard')&.api_key
+    api_secret = SmsSetting.find_by(sms_provider: 'SMS leopard')&.api_secret
+    
+
+    sms_template = ActsAsTenant.current_tenant.sms_template
+    # send_voucher_template = sms_template&.send_voucher_template
+    original_message =  "Hello, #{username} your invoice #{invoice_number}  for #{plan_name} has been generated and is due to expire on #{due_date}. Please renew your subscription to stay connected."
+
+    sender_id = "SMS_TEST"
+    uri = URI("https://api.smsleopard.com/v1/sms/send")
+    params = {
+      username: api_key,
+      password: api_secret,
+      message: original_message,
+      destination: phone_number,
+      source: sender_id
+    }
+    uri.query = URI.encode_www_form(params)
+
+    response = Net::HTTP.get_response(uri)
+    handle_sms_response(response, original_message, phone_number)
+  end
+
+
+
+  
+  def send_expiration_text_sms(phone_number, due_date, invoice_number, username, plan_name)
+    api_key = SmsSetting.find_by(sms_provider: 'TextSms')&.api_key
+    partnerID = SmsSetting.find_by(sms_provider: 'TextSms')&.partnerID
+
+    sms_template = ActsAsTenant.current_tenant.sms_template
+    # send_voucher_template = sms_template&.send_voucher_template
+        original_message =  "Hello, #{username} your invoice #{invoice_number}  for #{plan_name} has been generated and is due to expire on #{due_date}. Please renew your subscription to stay connected."
+
+
+    uri = URI("https://sms.textsms.co.ke/api/services/sendsms")
+    params = {
+      apikey: api_key,
+      message: original_message,
+      mobile: phone_number,
+      partnerID: partnerID,
+      shortcode: 'TextSMS',
+      sms_provider: 'TextSms'
+
+    }
+    uri.query = URI.encode_www_form(params)
+
+    response = Net::HTTP.get_response(uri)
+    handle_sms_response(response, original_message, phone_number)
+  end
+
+
+
+
+
+
+   def handle_sms_response(response, message, phone_number)
+    if response.is_a?(Net::HTTPSuccess)
+      sms_data = JSON.parse(response.body)
+      if sms_data['responses'] && sms_data['responses'][0]['respose-code'] == 200
+        sms_recipient = sms_data['responses'][0]['mobile']
+        sms_status = sms_data['responses'][0]['response-description']
+        
+        Rails.logger.info "Recipient: #{sms_recipient}, Status: #{sms_status}"
+
+        SystemAdminSm.create!(
+          user: sms_recipient,
+          message: message,
+          status: sms_status,
+          date: Time.current,
+          system_user: 'system',
+          sms_provider: 'SMS leopard'
+        )
+      else
+        Rails.logger.info "Failed to send message: #{sms_data['responses'][0]['response-description']}"
+      end
+    else
+      Rails.logger.error "Failed to send message: #{response.body}"
+    end
+  end
 
 
   
