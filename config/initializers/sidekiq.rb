fceecab08c4d107fad62f5bacffe6731362eb40c
malcomowilla <owillamalcom@gmail.com>
2025-10-31 10:57:31 +0300

    jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,71 +11,71 @@ Sidekiq.configure_server do |config|
         'cron' => '* * * * *'
       },
 
-      'rehydrate_wireguard_job' => {
-        'class' => 'RehydrateWireguardJob',
-        'cron' => '* * * * *'
-      },
+      # 'rehydrate_wireguard_job' => {
+      #   'class' => 'RehydrateWireguardJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'rad_sessions_job' => {
-        'class' => 'RadSessionsJob',
-        'every' => ['20s'],
-        'queue' => 'radacct'
-      },
+      # 'rad_sessions_job' => {
+      #   'class' => 'RadSessionsJob',
+      #   'every' => ['20s'],
+      #   'queue' => 'radacct'
+      # },
 
-      'generate_clients_conf_job' => {
-        'class' => 'GenerateClientsConfJob',
-        'cron' => '* * * * *'
-      },
+      # 'generate_clients_conf_job' => {
+      #   'class' => 'GenerateClientsConfJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'online_stats_broadcast_job' => {
-        'class' => 'OnlineStatsBroadcastJob',
-        'cron' => '* * * * *'
-      },
+      # 'online_stats_broadcast_job' => {
+      #   'class' => 'OnlineStatsBroadcastJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'restart_cloudflared_if_tunnel_missing_job' => {
-        'class' => 'RestartCloudflaredIfTunnelMissingJob',
-        'cron' => '* * * * *'
-      },
+      # 'restart_cloudflared_if_tunnel_missing_job' => {
+      #   'class' => 'RestartCloudflaredIfTunnelMissingJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'lock_account_job' => {
-        'class' => 'LockAccountJob',
-        'cron' => '*/2 * * * *'
-      },
+      # 'lock_account_job' => {
+      #   'class' => 'LockAccountJob',
+      #   'cron' => '*/2 * * * *'
+      # },
 
-      'hotspot_expiration_job' => {
-        'class' => 'HotspotExpirationJob',
-        'cron' => '* * * * *'
-      },
+      # 'hotspot_expiration_job' => {
+      #   'class' => 'HotspotExpirationJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'contention_ratio_job' => {
-        'class' => 'ContentionRatioJob',
-        'cron' => '* * * * *'
-      },
+      # 'contention_ratio_job' => {
+      #   'class' => 'ContentionRatioJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'router_ping_job' => {
-        'class' => 'RouterPingJob',
-        'cron' => '* * * * *'
-      },
+      # 'router_ping_job' => {
+      #   'class' => 'RouterPingJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'subscription_expiration_job' => {
-        'class' => 'SubscriptionExpirationJob',
-        'cron' => '* * * * *'
-      },
+      # 'subscription_expiration_job' => {
+      #   'class' => 'SubscriptionExpirationJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'system_metrics_job' => {
-        'class' => 'SystemMetricsJob',
-        'cron' => '* * * * *'
-      },
+      # 'system_metrics_job' => {
+      #   'class' => 'SystemMetricsJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'company_id_job' => {
-        'class' => 'CompanyIdJob',
-        'cron' => '* * * * *'
-      },
+      # 'company_id_job' => {
+      #   'class' => 'CompanyIdJob',
+      #   'cron' => '* * * * *'
+      # },
 
-      'inactivity_check_job' => {
-        'class' => 'InactivityCheckJob',
-        'cron' => '* * * * *'
-      }
+      # 'inactivity_check_job' => {
+      #   'class' => 'InactivityCheckJob',
+      #   'cron' => '* * * * *'
+      # }
     }
 
     Sidekiq.schedule = schedule
