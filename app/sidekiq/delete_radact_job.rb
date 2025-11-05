6a52588aa90573eaa389fc482a064dca7f0d6e21
malcomowilla <owillamalcom@gmail.com>
2025-11-05 15:32:56 +0300

    delete radct radcheck

--- app/sidekiq/delete_radact_job.rb
+++ app/sidekiq/delete_radact_job.rb
@@ -1,20 +1,31 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class DeleteRadactJob
   include Sidekiq::Job
   queue_as :default
 
   def perform
+    # Get all *existing* account_ids
     valid_account_ids = Account.pluck(:id)
 
+    # -------------------------------
+    # DELETE INVALID RadAcct ENTRIES
+    # -------------------------------
     RadAcct.unscoped.where.not(account_id: valid_account_ids).find_each do |radacct|
-      begin
-        Rails.logger.info "Deleting RadAcct ID=#{radacct.id}, invalid account_id=#{radacct.account_id}"
+      Rails.logger.info "Deleting RadAcct ID=#{radacct.id}, invalid account_id=#{radacct.account_id}"
+      radacct.destroy!
+      Rails.logger.info "Deleted RadAcct ID=#{radacct.id}"
+    end
 
-        radacct.destroy!   # This is where it's failing
-
-        Rails.logger.info "✅ Successfully deleted RadAcct ID=#{radacct.id}"
-      rescue => e
-        Rails.logger.info "❌ Failed to delete RadAcct ID=#{radacct.id}: #{e.class} - #{e.message}"
-      end
+    # -------------------------------
+    # DELETE INVALID Radcheck ENTRIES
+    # -------------------------------
+    Radcheck.where.not(account_id: valid_account_ids).find_each do |radcheck|
+      Rails.logger.info "Deleting Radcheck ID=#{radcheck.id}, invalid account_id=#{radcheck.account_id}"
+      radcheck.destroy!
+      Rails.logger.info "Deleted Radcheck ID=#{radcheck.id}"
     end
   end
 end
