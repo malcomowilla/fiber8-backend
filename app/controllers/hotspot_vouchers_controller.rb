d15aa2ce5263d6b2049ed94bedbb11cd6c664401
malcomowilla <owillamalcom@gmail.com>
2025-11-29 18:11:33 +0300

    hotspot

--- app/controllers/hotspot_vouchers_controller.rb
+++ app/controllers/hotspot_vouchers_controller.rb
@@ -227,7 +227,7 @@ end
       # user_manager_user_id = get_user_manager_user_id(@hotspot_voucher.voucher)
       # user_profile_id = get_user_profile_id_from_mikrotik(@hotspot_voucher.voucher)
       # calculate_expiration(package, hotspot_package_created)
-      create_voucher_radcheck(@hotspot_voucher.voucher, @hotspot_voucher.package, @hotspot_voucher.shared_users)
+      create_voucher_radcheck(@hotspot_voucher.voucher, @hotspot_voucher.package, params[:shared_users])
       # @hotspot_voucher.update(
       #   user_manager_user_id: user_manager_user_id,
       #     user_profile_id: user_profile_id,
