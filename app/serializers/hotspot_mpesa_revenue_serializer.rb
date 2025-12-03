ddc365589b97f9086d33e3a3d6e20f2bb065e715
malcomowilla <owillamalcom@gmail.com>
2025-12-03 18:15:26 +0300

    revenue

--- app/serializers/hotspot_mpesa_revenue_serializer.rb
+++ app/serializers/hotspot_mpesa_revenue_serializer.rb
@@ -1,3 +1,10 @@
 class HotspotMpesaRevenueSerializer < ActiveModel::Serializer
   attributes :id, :voucher, :payment_method, :amount, :reference, :time_paid, :account_id
+
+
+def time_paid
+  object.time_paid.strftime("%d %b %Y %H:%M:%S")
+end
+
+
 end
