afaeb47dff67ee1371a5d23f3c1a26e82b1cd385
malcomowilla <owillamalcom@gmail.com>
2025-12-31 10:02:21 +0300

    mpesa disbursement

--- app/controllers/disburse_payment_controller.rb
+++ app/controllers/disburse_payment_controller.rb
@@ -43,7 +43,7 @@ def set_tenant
     return render json: { error: "Unable to generate access token" }, status: :unprocessable_entity unless token
     # mpesa = ActsAsTenant.current_tenant.hotspot_mpesa_setting.find_by(account_type: "Paybill")
     mpesa = HotspotMpesaSetting.find_by(account_type: "Paybill")
-    Rails.logger.info "Mpesa: #{mpesa.shortcode}"
+    Rails.logger.info "Mpesa: #{mpesa.short_code}"
     Rails.logger.info "Mpesa: #{mpesa.consumer_key}"
     Rails.logger.info "Mpesa: #{mpesa.consumer_secret}"
     return render json: { error: "M-Pesa Settings Not Found" }, status: :not_found unless mpesa
