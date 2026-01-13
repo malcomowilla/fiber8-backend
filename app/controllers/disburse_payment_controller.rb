78f37dadbae1c2a968ad2ca4a34a8bfb594ef545
malcomowilla <owillamalcom@gmail.com>
2026-01-13 10:55:05 +0300

    test payment

--- app/controllers/disburse_payment_controller.rb
+++ app/controllers/disburse_payment_controller.rb
@@ -98,7 +98,7 @@ def fetch_access_token
     Rails.logger.info "Mpesa consumer key: #{consumer_key}"
     Rails.logger.info "Mpesa consumer secret: #{consumer_secret}"
 
-    api_url = "https://api.safaricom.co.ke/oauth/v1/generate"
+    api_url = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
     response = RestClient.get(api_url, {
       Authorization: "Basic #{Base64.strict_encode64("#{consumer_key}:#{consumer_secret}")}"
