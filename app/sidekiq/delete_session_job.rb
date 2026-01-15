39844d5ddab648b6c06b333f2b339a6d35ffb26f
malcomowilla <owillamalcom@gmail.com>
2026-01-15 13:35:07 +0300

    Hotspot Session

--- app/sidekiq/delete_session_job.rb
+++ app/sidekiq/delete_session_job.rb
@@ -27,11 +27,8 @@ class DeleteSessionJob
   def sync_sessions!
     Rails.logger.info "[DeleteSessionJob] Sync sessions started"
 
-    online_ips = RadAcct
-      .where(acctstoptime: nil)
-      .where(framedprotocol: [nil, ''])
-      .pluck(:framedipaddress)
-      .uniq
+    online_ips = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq
+      
 
    
 
@@ -48,15 +45,15 @@ class DeleteSessionJob
     Rails.logger.info "[DeleteSessionJob] Online IPs: #{online_ips.inspect}"
 
     updated_online = TemporarySession
-      .where(ip:  offline_ips)
-      .where(connected: false)
+      .where(ip:  online_ips)
+      .where(connected: true)
       .update_all(connected: true, updated_at: Time.current)
 
     Rails.logger.info "[DeleteSessionJob] Marked ONLINE: #{updated_online}"
 
     updated_offline = TemporarySession
-      .where.not(ip: online_ips)
-      .where(connected: true)
+      .where(ip: offline_ips)
+      .where(connected: false)
       .update_all(connected: false, updated_at: Time.current)
 
     Rails.logger.info "[DeleteSessionJob] Marked OFFLINE: #{updated_offline}"
