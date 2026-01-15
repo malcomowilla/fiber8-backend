7d1fa9c5b464a31a64315802d0f2487873a4a228
malcomowilla <owillamalcom@gmail.com>
2026-01-15 13:56:37 +0300

    Hotspot Session

--- app/sidekiq/delete_session_job.rb
+++ app/sidekiq/delete_session_job.rb
@@ -27,33 +27,27 @@ sync_sessions
   def sync_sessions
     Rails.logger.info "[DeleteSessionJob] Sync sessions started"
 
-    online_ips = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq
+    online_ips = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq.map(&:to_s)
       
 
    
 
-      offline_ips = RadAcct.where.not(acctstoptime: nil, framedprotocol: '').where.not('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq
+      offline_ips = RadAcct.where.not(acctstoptime: nil, framedprotocol: '').where.not('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq.map(&:to_s)
       
       
-  #     RadAcct
-  # .where(framedprotocol: [nil, ''])
-  # .where.not(acctstoptime: nil)
-  # .where.not(framedipaddress: [nil, ''])
-  # .pluck(:framedipaddress)
-  # .uniq
 
     Rails.logger.info "[DeleteSessionJob] Online IPs: #{online_ips.inspect}"
 
     updated_online = TemporarySession
       .where(ip:  online_ips)
-      .where(connected: true)
+      # .where(connected: true)
       .update_all(connected: true, updated_at: Time.current)
 
     Rails.logger.info "[DeleteSessionJob] Marked ONLINE: #{updated_online}"
 
     updated_offline = TemporarySession
       .where(ip: offline_ips)
-      .where(connected: false)
+      #  .where(connected: false)
       .update_all(connected: false, updated_at: Time.current)
 
     Rails.logger.info "[DeleteSessionJob] Marked OFFLINE: #{updated_offline}"
