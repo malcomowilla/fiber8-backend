cd404f89ff87d3205dd401f9215c4c1237f2f060
malcomowilla <owillamalcom@gmail.com>
2026-01-15 14:02:38 +0300

    Hotspot Session

--- app/sidekiq/generate_clients_conf_job.rb
+++ app/sidekiq/generate_clients_conf_job.rb
@@ -7,6 +7,35 @@ class GenerateClientsConfJob
 
   def perform
     Rails.logger.info "Generating clients.conf"
+
+ Rails.logger.info "[DeleteSessionJob] Sync sessions started"
+
+    online_ips = RadAcct.where(acctstoptime: nil, framedprotocol: '').where('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq.map(&:to_s)
+      
+
+   
+
+      offline_ips = RadAcct.where.not(acctstoptime: nil, framedprotocol: '').where.not('acctupdatetime > ?', 3.minutes.ago).pluck(:framedipaddress).uniq.map(&:to_s)
+      
+      
+
+    Rails.logger.info "[DeleteSessionJob] Online IPs: #{online_ips.inspect}"
+
+    updated_online = TemporarySession
+      .where(ip:  online_ips)
+      # .where(connected: true)
+      .update_all(connected: true, updated_at: Time.current)
+
+    Rails.logger.info "[DeleteSessionJob] Marked ONLINE: #{updated_online}"
+
+    updated_offline = TemporarySession
+      .where(ip: offline_ips)
+      #  .where(connected: false)
+      .update_all(connected: false, updated_at: Time.current)
+
+    Rails.logger.info "[DeleteSessionJob] Marked OFFLINE: #{updated_offline}"
+
+
     File.open('/etc/freeradius/3.0/clients.conf', 'w') do |f|
       Na.find_each do |nas|
         next if nas.nasname.blank? || nas.secret.blank?
