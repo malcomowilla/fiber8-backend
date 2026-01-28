105d91db874e371cef839f2bd4967ecaf28b41d4
malcomowilla <owillamalcom@gmail.com>
2026-01-28 16:15:57 +0300

    Hotspot Voucher

--- app/serializers/hotspot_voucher_serializer.rb
+++ app/serializers/hotspot_voucher_serializer.rb
@@ -5,7 +5,10 @@ class HotspotVoucherSerializer < ActiveModel::Serializer
 
 
   
+def last_logged_in
+  object.last_logged_in.strftime("%B %d, %Y at %I:%M %p") if object.last_logged_in.present?
 
+end
 
   # expiration_time&.strftime("%B %d, %Y at %I:%M %p")
 def expiration
