ae6140d4f980e2ae57d66ed9667f406e476b6157
malcomowilla <owillamalcom@gmail.com>
2026-02-08 00:42:15 +0300

    payments

--- app/serializers/pp_poe_mpesa_revenue_serializer.rb
+++ app/serializers/pp_poe_mpesa_revenue_serializer.rb
@@ -2,7 +2,10 @@ class PpPoeMpesaRevenueSerializer < ActiveModel::Serializer
   attributes :id, :payment_method, :amount, :reference,
    :time_paid, :account_id, :account_number, :payment_type
 
+def time_paid
+Time.zone.strptime(object.time_paid, "%Y%m%d%H%M%S").strftime("%B %d, %Y at %I:%M %p")
 
+end
 
 end
 
