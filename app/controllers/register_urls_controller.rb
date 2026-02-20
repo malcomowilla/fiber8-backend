97a81502ebd70a375d03aca836bb30ac80a93635
malcomowilla <owillamalcom@gmail.com>
2026-02-20 11:23:37 +0300

    voucher

--- app/controllers/register_urls_controller.rb
+++ app/controllers/register_urls_controller.rb
@@ -31,7 +31,7 @@ end
     token = fetch_access_token
     return render json: { error: "Unable to generate access token" }, status: :unprocessable_entity unless token
 
-    mpesa = HotspotMpesaSetting.find_by(account_type: "Paybill")
+    mpesa = ActsAsTenant.current_tenant.hotspot_mpesa_setting
     return render json: { error: "M-Pesa Settings Not Found" }, status: :not_found unless mpesa
  host = request.headers['X-Subdomain']
     payload = {
