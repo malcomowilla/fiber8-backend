5a19cd2d890b79121d41961e0c2d49c3252be5e3
malcomowilla <owillamalcom@gmail.com>
2026-02-22 18:14:10 +0300

    voucher

--- app/controllers/hotspot_vouchers_controller.rb
+++ app/controllers/hotspot_vouchers_controller.rb
@@ -3,7 +3,8 @@ class HotspotVouchersController < ApplicationController
 
 load_and_authorize_resource except: [:login_with_hotspot_voucher,
  :make_payment, :check_payment_status, :payment_and_conected_status,
- :stk_push_status, :transaction_status_result
+ :stk_push_status, :transaction_status_result,:login_with_receipt_number,
+ :receipt_number_status
 
 ]
   # skip_before_action :set_tenant, only: [:check_payment_status]
