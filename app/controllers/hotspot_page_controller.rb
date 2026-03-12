6565ca7a277ece24fb02776c6b3c82e4a98312b6
malcomowilla <owillamalcom@gmail.com>
2026-03-12 22:58:15 +0300

    caching

--- app/controllers/hotspot_page_controller.rb
+++ app/controllers/hotspot_page_controller.rb
@@ -1,6 +1,6 @@
 
 class HotspotPageController < ApplicationController
-  def data
+   def data
     subdomain = request.headers["X-Subdomain"]
     account = Account.find_by(subdomain: subdomain)
 
@@ -8,17 +8,11 @@ class HotspotPageController < ApplicationController
 
     data = Rails.cache.fetch("hotspot_page_data_#{account.id}", expires_in: 5.minutes) do
       {
-        templates: account.hotspot_templates,
-        ads: account.ads,
-        ad_settings: account.ad_setting,
-        hotspot_settings: account.hotspot_setting,
-        customization: account.hotspot_customization,
-        # company: {
-        #   company_name: account.company_name,
-        #   contact_info: account.contact_info,
-        #   email_info: account.email_info,
-        #   logo_url: account.logo_url
-        # }
+        templates: account.hotspot_templates.as_json,
+        ads: account.ads.as_json,
+        ad_settings: account.ad_setting&.as_json,
+        hotspot_settings: account.hotspot_setting&.as_json,
+        customization: account.hotspot_customization&.as_json
       }
     end
 
