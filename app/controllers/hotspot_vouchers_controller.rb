56c70c5ed18b15c2342102db9f150d9675416a5f
malcomowilla <owillamalcom@gmail.com>
2026-03-19 18:28:37 +0300

    optimization

--- app/controllers/hotspot_vouchers_controller.rb
+++ app/controllers/hotspot_vouchers_controller.rb
@@ -401,7 +401,7 @@ if present_voucher_or_username
     if response.code == 200
  
 
-   HotspotMpesaRevenue.find_by(reference: transaction_id).hotspot_voucher.update!(status: "used",
+   HotspotMpesaRevenue.find_by(reference: transaction_id).hotspot_voucher.update!(status:"used",
    login_by:'Trasnsaction Code', 
       last_logged_in: Time.now,
        ip: HotspotMpesaRevenue.find_by(reference: transaction_id).hotspot_voucher.ip, used_voucher: true)
@@ -522,10 +522,6 @@ end
 def check_payment_status
   raw_body = request.body.read
 
-# Rails.logger.info "Parsed data calback mpesa: #{request.body.read}"
-# Rails.logger.info "Parsed data callback mpesa: #{raw_body}"
-
-  # data = JSON.parse(request.body.read) rescue {}
   data = JSON.parse(raw_body) rescue {}
   bill_ref = data["BillRefNumber"]
 
