541ebf54a2deaf3cc9826ee92e620513dc86ad3c
malcomowilla <owillamalcom@gmail.com>
2026-03-24 20:46:49 +0300

    compensation

--- app/sidekiq/delete_radact_job.rb
+++ app/sidekiq/delete_radact_job.rb
@@ -7,17 +7,17 @@ class DeleteRadactJob
   queue_as :default
 
   def perform
-    # Get all *existing* account_ids
-    valid_account_ids = Account.pluck(:id)
+    # # Get all *existing* account_ids
+    # valid_account_ids = Account.pluck(:id)
 
-    # -------------------------------
-    # DELETE INVALID RadAcct ENTRIES
-    # -------------------------------
-    RadAcct.unscoped.where.not(account_id: valid_account_ids).find_each do |radacct|
-      Rails.logger.info "Deleting RadAcct ID=#{radacct.id}, invalid account_id=#{radacct.account_id}"
-      radacct.destroy!
-      Rails.logger.info "Deleted RadAcct ID=#{radacct.id}"
-    end
+    # # -------------------------------
+    # # DELETE INVALID RadAcct ENTRIES
+    # # -------------------------------
+    # RadAcct.unscoped.where.not(account_id: valid_account_ids).find_each do |radacct|
+    #   Rails.logger.info "Deleting RadAcct ID=#{radacct.id}, invalid account_id=#{radacct.account_id}"
+    #   radacct.destroy!
+    #   Rails.logger.info "Deleted RadAcct ID=#{radacct.id}"
+    # end
 
 
 
@@ -25,11 +25,11 @@ class DeleteRadactJob
  # -------------------------------
     # DELETE INVALID RadReply ENTRIES
     # -------------------------------
-    RadReply.where.not(account_id: valid_account_ids).find_each do |radreply|
-      Rails.logger.info "Deleting RadReply ID=#{radreply.id}, invalid account_id=#{radreply.account_id}"
-      radreply.destroy!
-      Rails.logger.info "Deleted RadReply ID=#{radreply.id}"
-    end
+    # RadReply.where.not(account_id: valid_account_ids).find_each do |radreply|
+    #   Rails.logger.info "Deleting RadReply ID=#{radreply.id}, invalid account_id=#{radreply.account_id}"
+    #   radreply.destroy!
+    #   Rails.logger.info "Deleted RadReply ID=#{radreply.id}"
+    # end
 
 
 
@@ -39,10 +39,10 @@ class DeleteRadactJob
     # -------------------------------
     # DELETE INVALID Radcheck ENTRIES
     # -------------------------------
-    RadCheck.where.not(account_id: valid_account_ids).find_each do |radcheck|
-      Rails.logger.info "Deleting Radcheck ID=#{radcheck.id}, invalid account_id=#{radcheck.account_id}"
-      radcheck.destroy!
-      Rails.logger.info "Deleted Radcheck ID=#{radcheck.id}"
-    end
+    # RadCheck.where.not(account_id: valid_account_ids).find_each do |radcheck|
+    #   Rails.logger.info "Deleting Radcheck ID=#{radcheck.id}, invalid account_id=#{radcheck.account_id}"
+    #   radcheck.destroy!
+    #   Rails.logger.info "Deleted Radcheck ID=#{radcheck.id}"
+    # end
   end
 end
