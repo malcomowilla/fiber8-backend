1ef02ea2158314142e33936193eb1a7367c8af9b
malcomowilla <owillamalcom@gmail.com>
2026-04-06 10:25:13 +0300

    hotspot expiration

--- app/serializers/hotspot_mpesa_revenue_serializer.rb
+++ app/serializers/hotspot_mpesa_revenue_serializer.rb
@@ -1,11 +1,19 @@
 class HotspotMpesaRevenueSerializer < ActiveModel::Serializer
   attributes :id, :voucher, :payment_method, :amount, :reference, 
-  :time_paid,  :name, :phone_number
+  :time_paid,  :name, :phone_number, :paid_out, :paid_out_at
+
+
 
 
   # Return the time_paid in the format "%B %d, %Y at %I:%M %p"
 def time_paid
 Time.zone.strptime(object.time_paid, "%Y%m%d%H%M%S").strftime("%B %d, %Y at %I:%M %p")
+
+end
+
+
+def paid_out_at
+    object.paid_out_at.strftime("%B %d, %Y at %I:%M %p") if object.paid_out_at.present?
 
 end
 
