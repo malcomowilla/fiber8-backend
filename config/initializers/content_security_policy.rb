59f50ed98e3722aed1bb681b7486d16de066b87e
malcomowilla <owillamalcom@gmail.com>
2026-04-08 23:24:29 +0300

    policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,8 +4,10 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
+Rails.application.config.content_security_policy do |policy|
+
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
