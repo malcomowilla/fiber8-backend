8de0ad5bc11f89446992e1d1b8b302064ddb4520
malcomowilla <owillamalcom@gmail.com>
2026-05-11 14:52:40 +0300

    free trial for hotspot users

--- app/serializers/hotspot_customization_serializer.rb
+++ app/serializers/hotspot_customization_serializer.rb
@@ -1,10 +1,7 @@
 class HotspotCustomizationSerializer < ActiveModel::Serializer
   attributes :id, :customize_template_and_package_per_location, 
   :enable_autologin, :compensation_minutes, :compensation_hours,
-  :enable_compensation, :enable_free_trial,
-    :free_trial_duration_minutes,
-    :free_trial_download_limit,
-    :free_trial_upload_limit
+  :enable_compensation
 end
 
 
