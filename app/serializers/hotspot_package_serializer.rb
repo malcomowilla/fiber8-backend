6a603f43a2aad5c20d42c6653e55f74652f842d9
malcomowilla <owillamalcom@gmail.com>
2026-06-02 20:14:38 +0300

    system charges

--- app/serializers/hotspot_package_serializer.rb
+++ app/serializers/hotspot_package_serializer.rb
@@ -1,7 +1,7 @@
 class HotspotPackageSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :download_limit, :upload_limit, :account_id, :tx_rate_limit, :rx_rate_limit, 
   :validity_period_units, :download_burst_limit, :upload_burst_limit, :validity, :speed, :valid,
-  :valid_from, :valid_until, :weekdays, :shared_users, :location, :package_speed,
+  :valid_from, :valid_until, :weekdays, :location, :package_speed,
    :burst_enabled,
     :burst_limit_download,
     :burst_limit_upload,
