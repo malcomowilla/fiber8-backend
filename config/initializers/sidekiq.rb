bc5a64f5cda8fbb5a71c9036e53866089a95b5ac
malcomowilla <owillamalcom@gmail.com>
2026-06-20 12:11:46 +0300

    hotspot sessions

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -92,7 +92,7 @@ require 'sidekiq-scheduler'
 require "sidekiq-unique-jobs"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 
   # ✅ This automatically loads config/sidekiq.yml schedules
   # 
@@ -113,7 +113,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 
    config.client_middleware do |chain|
     chain.add SidekiqUniqueJobs::Middleware::Client
