93d6ade37e1916e6db41dc09398f0e8774cf61ea
malcomowilla <owillamalcom@gmail.com>
2026-06-28 19:17:28 +0300

    fup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -92,7 +92,7 @@ require 'sidekiq-scheduler'
 require "sidekiq-unique-jobs"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: 'redis://redis:6379/0' }
 
   # ✅ This automatically loads config/sidekiq.yml schedules
   # 
