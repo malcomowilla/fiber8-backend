e48fc46bb2b2f68564107fa80f12a9a50f900cb8
malcomowilla <owillamalcom@gmail.com>
2026-06-30 18:22:42 +0300

    fup

--- app/serializers/pp_poe_mpesa_revenue_serializer.rb
+++ app/serializers/pp_poe_mpesa_revenue_serializer.rb
@@ -4,16 +4,15 @@ class PpPoeMpesaRevenueSerializer < ActiveModel::Serializer
    :customer_name,  :paid_out, :paid_out_at, 
   :amount_disbursed, :status
 
-# def time_paid
-#  object.time_paid.strftime("%B %d, %Y at %I:%M %p")
-# end
-
-
 def time_paid
-  return nil if object.time_paid.blank?
-
-  Time.strptime(object.time_paid, "%Y%m%d%H%M%S")
-      .strftime("%d %b %Y %I:%M %p")
+ object.time_paid.strftime("%B %d, %Y at %I:%M %p")
 end
 
+
+
 end
+
+
+
+
+
