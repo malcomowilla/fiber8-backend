4f82a23f3ce233da65c8a9835f9c3dacf022d291
malcomowilla <owillamalcom@gmail.com>
2026-07-11 17:38:26 +0300

    hotspot

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,28 @@
-# config/initializers/cors.rb
+# # config/initializers/cors.rb
+
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins '*'
+#       # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+#          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+
+#       credentials: false  # Add this line if you're using cookies or session-based authentication
+
+#     end
+#   end
+
+
+
+
+
+
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      # resource '*', headers: :any, methods: [:get, :post, :patch, :put],
-         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-
-      credentials: false  # Add this line if you're using cookies or session-based authentication
-
-    end
+  allow do
+    origins '*'
+    resource '/api/*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options],
+      credentials: false
   end
-
+end
