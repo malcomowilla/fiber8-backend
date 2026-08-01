6c9bceea4f121e478dff31a8be5e89a555abc05d
malcomowilla <owillamalcom@gmail.com>
2026-08-01 18:32:22 +0300

    leads

--- config/routes.rb
+++ config/routes.rb
@@ -188,6 +188,8 @@ resources :pops
 resources :network_devices
 resources :network_connections
 get '/invoice_payments', to: 'system_admins#invoice_payments'
+get '/api/invoice_payments', to: 'system_admins#invoice_payments'
+
 post '/api/nas_routers/:id/remote_winbox_session', to: 'nas_routers#remote_winbox_session'
 post '/api/payment_and_conected_status', to: 'hotspot_vouchers#payment_and_conected_status'
 
