bb6f3f087381354eb2cf77cc6362623c9c3f4906
malcomowilla <owillamalcom@gmail.com>
2026-08-16 02:56:55 +0300

    Expiration Reminder

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ end
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-workers 0
+workers 1
 threads 5,5
 preload_app!
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
