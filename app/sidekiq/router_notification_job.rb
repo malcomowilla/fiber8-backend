c63147dd4f6e8d674379c22c2629f6f276354372
malcomowilla <owillamalcom@gmail.com>
2026-08-18 02:46:35 +0300

    kwani ni kesho

--- app/sidekiq/router_notification_job.rb
+++ app/sidekiq/router_notification_job.rb
@@ -3,8 +3,8 @@ require 'open3'
 class RouterNotificationJob
   include Sidekiq::Job
   queue_as :default
-  sidekiq_options lock: :until_executed, lock_timeout: 0
-
+  # sidekiq_options lock: :until_executed, lock_timeout: 0
+  sidekiq_options lock: :while_executing  # was: lock: :until_executed, lock_timeout: 0
   def perform
     Account.find_each do |tenant|
       ActsAsTenant.with_tenant(tenant) do
