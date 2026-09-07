35d7b9cd7b13077c19b305d3f033f4b7e1159acc
malcomowilla <owillamalcom@gmail.com>
2026-09-07 17:57:19 +0300

    sms templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,13 @@ Rails.application.configure do
   config.enable_reloading = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILTAP_SMTP_HOST'],
     port:                 587,
     user_name:            ENV['MAILTAP_API_USERNAME'],
     password:             ENV['MAILTAP_API_TOKENS'],
-    domain:               'aitechs.co.ke',
+    domain:               'owitech.co.ke',
     authentication:       :plain,
     enable_starttls_auto: true
   }
